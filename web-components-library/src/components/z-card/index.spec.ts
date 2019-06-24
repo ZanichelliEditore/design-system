5830b23d3963ccec25e5b9890b9990b3a90811cf
Giulio Paoli <giulio.paoli@gruppometa.it>
2019-06-24 15:18:27 +0200

    MYZ-6841 aggiunti primi test unitari sulla card

---
+++ web-components-library/src/components/z-card/index.spec.ts
@@ -0,0 +1,22 @@
+import { newSpecPage } from "@stencil/core/testing";
+
+import { ZCardComponent } from "./index";
+
+describe("Suite test ZCard", () => {
+  it("Test render ZCard vuota", async () => {
+    const page = await newSpecPage({
+      components: [ZCardComponent],
+      html: `<z-card></z-card>`
+    });
+
+    expect(page.root).toEqualHtml(`
+    <z-card>
+      <mock:shadow-root>
+        <div>
+          <slot></slot>
+        </div>
+      </mock:shadow-root>
+    </z-card>
+    `);
+  });
+});
