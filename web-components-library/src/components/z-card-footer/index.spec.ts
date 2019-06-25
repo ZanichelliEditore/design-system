736107ac6ec5fd329e43f0db5235d3d8339f878c
pasquale <pasquale.merolle@thinkopen.it>
2019-06-25 12:09:25 +0200

    MYZ-6841 added tests

--- web-components-library/src/components/z-card-footer/index.spec.ts
+++ web-components-library/src/components/z-card-footer/index.spec.ts
@@ -1,5 +1,7 @@
 import { newSpecPage } from "@stencil/core/testing";
 
+import { shallow, mount, render } from "enzyme";
+
 import { ZCardFooter } from "./index";
 
 describe("Suite test ZCardFooter", () => {
@@ -33,7 +35,7 @@ describe("Suite test ZCardFooter", () => {
     `);
   });
 
-  it("Test render ZCardFooter con props", async () => {
+  it("Test render ZCardFooter senza bottone", async () => {
     const page = await newSpecPage({
       components: [ZCardFooter],
       html: `<z-card-footer titolo="titolo" autori="Mario Rossi, Paolo Bianchi" anno="2019"></z-card-footer>`
@@ -51,6 +53,33 @@ describe("Suite test ZCardFooter", () => {
             </p>
             <p>
               Edizione: <b>2019</b>
+            </p>
+          </div>
+          <slot name="list" />
+        </footer>
+      </mock:shadow-root>
+    </z-card-footer>
+    `);
+  });
+
+  it("Test render ZCardFooter senza dati", async () => {
+    const page = await newSpecPage({
+      components: [ZCardFooter],
+      html: `<z-card-footer></z-card-footer>`
+    });
+
+    expect(page.root).toEqualHtml(`
+    <z-card-footer>
+      <mock:shadow-root>
+        <footer >
+
+          <h2></h2>
+          <div>
+            <p>
+              Autore: <b></b>
+            </p>
+            <p>
+              Edizione: <b></b>
             </p>
           </div>
           <slot name="list" />
