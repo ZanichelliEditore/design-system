34c05614acee9cc284944a9277400cd01b123c5a
pasquale <pasquale.merolle@thinkopen.it>
2019-07-03 12:56:35 +0200

    MYZ-6871 styling and javascripting

--- web-components-library/src/components/z-header/index.tsx
+++ web-components-library/src/components/z-header/index.tsx
@@ -28,7 +28,12 @@ export class ZHeader {
 
   handleHomeButtonClick(): void {
     const dropdown = this.element.shadowRoot.getElementById("dropdown-menu");
-    dropdown.classList.toggle('visible')
+    dropdown.classList.toggle("visible");
+  }
+
+  handleDizionariButtonClick(): void {
+    const dropdown = this.element.shadowRoot.getElementById("dropdown-menu");
+    dropdown.classList.remove("visible");
   }
 
   render() {
@@ -42,27 +47,56 @@ export class ZHeader {
             <span>Testo a caso</span>
           </div>
         </div>
-        <div id="main-header" class={`main-header ${this.isSticky && 'sticky'}`}>
+        <div
+          id="main-header"
+          class={`main-header ${this.isSticky && "sticky"}`}
+        >
           <div class="logo">
             <slot name="logo" />
           </div>
           <div class="link-int">
-            <span class="menu-item" onClick={() => this.handleHomeButtonClick()}>Home</span>
-            <span class="menu-item">Dizionari</span>
+            <span>
+              <a
+                href="#home"
+                id="home"
+                class="menu-item"
+                onClick={() => this.handleHomeButtonClick()}
+              >
+                Home
+              </a>
+              <svg height="8" width="16" class="hidden">
+                <polygon points="8,0 16,8 0,8" class="arrow" />
+              </svg>
+            </span>
+            <span>
+              <a
+                href="#dizionari"
+                id="dizionari"
+                class="menu-item"
+                onClick={() => this.handleDizionariButtonClick()}
+              >
+                Dizionari
+              </a>
+              <svg height="8" width="16" class="hidden">
+                <polygon points="8,0 16,8 0,8" class="arrow" />
+              </svg>
+            </span>
           </div>
-          <div class="void"></div>
+          <div class="void" />
           <div class="link-ext">
             <a href="#supporto">Supporto</a>
             <a href="#shop">E-shop</a>
             <a href="#chiedi">Chiedi al tuo responsabile</a>
           </div>
-          <div class={`login ${this.isSticky && 'sticky'}`}>
+          <div class={`login ${this.isSticky && "sticky"}`}>
             <slot name="login" />
           </div>
         </div>
-        <div id="dropdown-menu" class={`dropdown-menu ${this.isSticky && 'sticky'}`}>
+        <div
+          id="dropdown-menu"
+          class={`dropdown-menu ${this.isSticky && "sticky"}`}
+        >
           <div class="dropdown-links">
-            <div class="arrow" />
             <a href="#libreria" class="active">
               La mia Libreria
             </a>
