dd61235d213e57064275ef2c46a5251b5abcbfb6
Valentina Pasquino <valentina.pasquino@thinkopen.it>
2019-07-04 12:36:40 +0200

    MYZ-6867 rimossa classe tertiary

--- web-components-library/src/components/z-link/index.tsx
+++ web-components-library/src/components/z-link/index.tsx
@@ -9,22 +9,13 @@ export class ZLink {
   @Prop() url: string;
   @Prop() label?: string;
   @Prop() icon?: string;
-  @Prop() type?: string
   @Prop() isdisabled?: boolean = false;
 
   render() {
-    let linkClass = '';
-    if(this.type) {
-      linkClass += this.type;
-    }
-    if(this.isdisabled) {
-      linkClass += ' disabled';
-    }
-
     const icon = this.icon ? '../../../assets/images/png/' + this.icon : '';
 
     return (
-      <a href={this.url} class={linkClass}>
+      <a href={this.url} class={this.isdisabled && 'disabled'}>
         {this.icon && <img src={icon} />}
         {this.label}
       </a>
