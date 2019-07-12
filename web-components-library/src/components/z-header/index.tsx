61473371d7ed396686b39a4948fb7e10b53e8beb
pasquale <pasquale.merolle@thinkopen.it>
2019-07-12 10:43:07 +0200

    MYZ-6781 refactoring

--- web-components-library/src/components/z-header/index.tsx
+++ web-components-library/src/components/z-header/index.tsx
@@ -24,24 +24,29 @@ export class ZHeader {
     const sticky = navbar.offsetTop;
     this.handleStickyNav(sticky);
 
-    const links =  this.element.shadowRoot.querySelectorAll(".dropdown-links > a[href^='#']");
+    const links = this.element.shadowRoot.querySelectorAll(
+      ".dropdown-links > a[href^='#']"
+    );
     // TODO: move to prop()
-    const sections = document.querySelectorAll('section');
+    const sections = document.querySelectorAll("section");
     let index = sections.length;
 
-    while(--index && window.scrollY + 50 < sections[index].offsetTop) {}
+    while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
 
-    links.forEach((link) => {
-      link.classList.remove('active')
+    links.forEach(link => {
+      link.classList.remove("active");
     });
-    links[index].classList.add('active');
+    links[index].classList.add("active");
   }
 
   componentWillLoad() {
     this.intMenuData = JSON.parse(this.intlinkdata);
     this.extMenuData = JSON.parse(this.extlinkdata);
     console.log(this.extMenuData);
-    this.activeMenuItem = this.intMenuData.filter((menu: HeaderIntLink) => window.location.hash === menu.url)[0] || this.intMenuData[0];
+    this.activeMenuItem =
+      this.intMenuData.filter(
+        (menu: HeaderIntLink) => window.location.hash === menu.url
+      )[0] || this.intMenuData[0];
   }
 
   handleStickyNav(sticky): void {
@@ -61,6 +66,29 @@ export class ZHeader {
   //   linkInt.classList.toggle("open");
   // }
 
+  renderTopHeader(): HTMLDivElement {
+    return (
+      <div class="top-header">
+        <div class="editors">
+          <slot name="editors" />
+        </div>
+      </div>
+    );
+  }
+
+  renderLogoDiv(): HTMLDivElement {
+    return (
+      <div class="logo">
+        <z-logo
+          link="https:\\www.zanichelli.it"
+          targetblank={true}
+          imageurl="../../assets/images/png/zanichelli-logo-2.png"
+          imagealt="logo zanichelli"
+        />
+      </div>
+    );
+  }
+
   renderIntMenu(menuItems: HeaderIntLink[]): HTMLDivElement {
     return (
       <div id="link-int" class="link-int">
@@ -76,12 +104,22 @@ export class ZHeader {
           href={menuItem.url}
           id={menuItem.id}
           class="menu-item"
-          onClick={() => { this.activeMenuItem = menuItem; }}
+          onClick={() => {
+            this.activeMenuItem = menuItem;
+          }}
         >
           <span>{menuItem.name}</span>
           <i />
         </a>
-        <svg height="8" width="16" class={ this.activeMenuItem ? menuItem.id !== this.activeMenuItem.id && "hidden" : "hidden"}>
+        <svg
+          height="8"
+          width="16"
+          class={
+            this.activeMenuItem
+              ? menuItem.id !== this.activeMenuItem.id && "hidden"
+              : "hidden"
+          }
+        >
           <polygon points="8,0 16,8 0,8" class="arrow" />
         </svg>
       </span>
@@ -93,14 +131,14 @@ export class ZHeader {
       return;
     }
     return (
-        <div
-          id="dropdown-menu"
-          class={`dropdown-menu ${this.isSticky && "sticky"}`}
-        >
+      <div
+        id="dropdown-menu"
+        class={`dropdown-menu ${this.isSticky && "sticky"}`}
+      >
         <div class="dropdown-links">
-          {menuItem.subMenu.map(item =>
+          {menuItem.subMenu.map(item => (
             <a href={item.url}>{item.name}</a>
-          )}
+          ))}
         </div>
       </div>
     );
@@ -109,12 +147,69 @@ export class ZHeader {
   renderExtMenu(menuItems: HeaderExtLink[]): HTMLDivElement {
     return (
       <div id="link-ext" class="link-ext">
-        {menuItems.map((menuItem: HeaderExtLink): HTMLSpanElement => {
-          const {id, name, url, icon} = menuItem;
-          return (
-            <span class="link-ext-span"><z-link id={id} url={url} label={name} icon={icon} iswhite={true} /></span>
-          )
-        })}
+        {menuItems.map(
+          (menuItem: HeaderExtLink): HTMLSpanElement => {
+            const { id, name, url, icon } = menuItem;
+            return (
+              <span class="link-ext-span">
+                <z-link
+                  id={id}
+                  url={url}
+                  label={name}
+                  icon={icon}
+                  iswhite={true}
+                />
+              </span>
+            );
+          }
+        )}
+      </div>
+    );
+  }
+
+  renderLoginDiv(): HTMLDivElement {
+    return (
+      <div class="login">
+        <z-menu-dropdown
+          nomeutente="Dario Docente"
+          menucontent='[{"text":"Profilo", "link":"http://www.zanichelli.it"},{"text":"Esci", "link":"http://www.google.it"}]'
+        />
+      </div>
+    );
+  }
+
+  renderMainHeader(): HTMLDivElement {
+    return (
+      <div
+        id="main-header"
+        class={`main-header ${this.isSticky && "sticky"}`}
+      >
+        {this.renderLogoDiv()}
+        {this.renderMobileMenu()}
+        {this.renderIntMenu(this.intMenuData)}
+        <div id="mobile-dropdown-d" class="mobile-dropdown">
+          <z-button label="Scarica la app BookTab" />
+        </div>
+
+        {this.renderExtMenu(this.extMenuData)}
+        {this.renderLoginDiv()}
+      </div>
+    )
+  }
+
+  renderMobileMenu(): HTMLDivElement {
+    return (
+      <div
+        class="menu"
+        id="mobile-menu-wrapper"
+        // onClick={() => this.handleMobileButtonClick()}
+      >
+        <span>Menu</span>
+        <div class="menu-toggle" id="mobile-menu">
+          <span class="bar" />
+          <span class="bar" />
+          <span class="bar" />
+        </div>
       </div>
     )
   }
@@ -122,54 +217,9 @@ export class ZHeader {
   render() {
     return (
       <header>
-        <div class="top-header">
-          <div class="editors">
-            <slot name="editors" />
-          </div>
-        </div>
-        <div
-          id="main-header"
-          class={`main-header ${this.isSticky && "sticky"}`}
-        >
-          <div class="logo">
-            <z-logo
-              link="https:\\www.zanichelli.it"
-              targetblank={true}
-              imageurl="../../assets/images/png/zanichelli-logo-2.png"
-              imagealt="logo zanichelli"
-            />
-          </div>
-          <div
-            class="menu"
-            id="mobile-menu-wrapper"
-            // onClick={() => this.handleMobileButtonClick()}
-          >
-            <span>Menu</span>
-            <div class="menu-toggle" id="mobile-menu">
-              <span class="bar" />
-              <span class="bar" />
-              <span class="bar" />
-            </div>
-          </div>
-
-          {this.renderIntMenu(this.intMenuData)}
-          <div id="mobile-dropdown-d" class="mobile-dropdown">
-            <z-button label="Scarica la app BookTab" />
-          </div>
-
-          {this.renderExtMenu(this.extMenuData)}
-
-          <div class="login">
-            <z-menu-dropdown
-              nomeutente="Dario Docente"
-              menucontent='[{"text":"Profilo", "link":"http://www.zanichelli.it"},{"text":"Esci", "link":"http://www.google.it"}]'
-            />
-          </div>
-
-        </div>
-
+        {this.renderTopHeader()}
+        {this.renderMainHeader()}
         {this.renderSubMenu(this.activeMenuItem)}
-
       </header>
     );
   }
