1c30fdede4eab925b5f3684279738361d56709f6
Valentina Pasquino <valentina.pasquino@thinkopen.it>
2019-07-22 16:02:58 +0200

    MYZ-6871 test z-header desktop

--- web-components-library/src/components/z-header/index.spec.ts
+++ web-components-library/src/components/z-header/index.spec.ts
@@ -6,7 +6,7 @@ describe("Suite test ZHeader", () => {
   /*
   TEST MOBILE
    */
-  it("Test render ZHeader myz utente non loggato", async () => {
+  it("Test render ZHeader mobile myz utente non loggato", async () => {
     const page = await newSpecPage({
       components: [ZHeader],
       html: initHeader(false, false, true, false, false)
@@ -32,7 +32,7 @@ describe("Suite test ZHeader", () => {
   `);
   });
 
-  it("Test render ZHeader non myz utente non loggato", async () => {
+  it("Test render ZHeader mobile non myz utente non loggato", async () => {
     const page = await newSpecPage({
       components: [ZHeader],
       html: initHeader(true, false, true, false, false)
@@ -58,7 +58,7 @@ describe("Suite test ZHeader", () => {
   `);
   });
 
-  it("Test render ZHeader myz loggato", async () => {
+  it("Test render ZHeader mobile myz loggato", async () => {
     const page = await newSpecPage({
       components: [ZHeader],
       html: initHeader(true, true, true, false, false)
@@ -118,6 +118,271 @@ describe("Suite test ZHeader", () => {
 
   /*
   TEST MOBILE END
+  */
+
+  /*
+  TEST DESKTOP
+  */
+
+  it("Test render ZHeader myz vuoto", async () => {
+    const page = await newSpecPage({
+      components: [ZHeader],
+      html: initHeader(false, false, true, false, false)
+    });
+    expect(page.root).toEqualHtml(`
+    <z-header extlinkdata='[]' intlinkdata='[]' ismyz="true">
+      <mock:shadow-root>
+        <header>
+          <div class="top-header">
+            <div class="editors">
+              <slot name="editors"></slot>
+            </div>
+          </div>
+          <div class="main-header false false" id="main-header">
+            <div class="logo">
+              <z-logo imagealt="logo zanichelli" imageurl="../../assets/images/png/zanichelli-logo-2.png" link="https://www.zanichelli.it" targetblank></z-logo>
+            </div>
+            <div></div>
+            <div></div>
+            <div class="login">
+              <z-button buttonid="login-button" label="entra" type="secondary"></z-button>
+            </div>
+          </div>
+          <div class="dropdown-menu hidden false" id="dropdown-menu"></div>
+        </header>
+      </mock:shadow-root>
+    </z-header>
+  `);
+  });
+
+  it("Test render ZHeader myz loggato", async () => {
+    const page = await newSpecPage({
+      components: [ZHeader],
+      html: initHeader(true, true, true, true, true)
+    });
+    expect(page.root).toEqualHtml(`
+    <z-header
+      intlinkdata='[{"id":"home","name":"Home","url":"#home","subMenu":[{"id":"libreria","name":"la mia libreria","url":"#libreria"}]},{"id":"Dizionari","name":"Dizionari","url":"https://www.zanichelli.it/dizionari/in-primo-piano"}]'
+      extlinkdata='[{"id":"supporto","name":"Supporto","url":"https://www.zanichelli.it/contatti-e-recapiti","icon":"question-mark.png"}]'
+      userdata='{"islogged":true,"id":123456,"name":"Dario Docente e Professore","usergroup":15}'
+      ismyz="true"
+    >
+      <mock:shadow-root>
+        <header>
+          <div class="top-header">
+            <div class="editors">
+              <slot name="editors" />
+            </div>
+          </div>
+          <div id="main-header" class="main-header false false">
+            <div class="logo">
+              <z-logo
+                link="https://www.zanichelli.it"
+                targetblank
+                imageurl="../../assets/images/png/zanichelli-logo-2.png"
+                imagealt="logo zanichelli"
+              />
+            </div>
+            <div id="link-int" class="link-int">
+              <span>
+                <a href="#home" id="home" class="menu-item">
+                  <span>Home</span>
+                  <i></i>
+                </a>
+                <svg height="8" width="16"><polygon points="8,0 16,8 0,8" class="arrow"></polygon></svg>
+                <span class="mobile-dropdown" id="mobile-dropdown-home">
+                  <z-list></z-list>
+                </span>
+              </span>
+              <span>
+                <a id="Dizionari" class="menu-item" href="https://www.zanichelli.it/dizionari/in-primo-piano">
+                  <span>Dizionari</span>
+                </a>
+                <svg height="8" width="16" class="hidden"><polygon points="8,0 16,8 0,8" class="arrow"></polygon></svg>
+              </span>
+            </div>
+            <div id="link-ext" class="link-ext">
+              <span class="link-ext-span myz">
+                <z-link id="supporto" url="https://www.zanichelli.it/contatti-e-recapiti" label="Supporto" icon="question-mark.png" iswhite target="_blank" />
+              </span>
+            </div>
+            <div class="login">
+              <z-menu-dropdown
+                nomeutente="Dario Docente e Professore"
+                menucontent='[{"text":"Profilo", "link":"http://www.zanichelli.it"},{"text":"Esci", "link":"#home", "linkid":"logout-button"}]'
+              ></z-menu-dropdown>
+            </div>
+          </div>
+          <div id="dropdown-menu" class="dropdown-menu false">
+            <div class="dropdown-links">
+              <a class="active" href="#libreria">
+                la mia libreria
+              </a>
+            </div>
+          </div>
+        </header>
+      </mock:shadow-root>
+    </z-header>
+  `);
+  });
+
+  it("Test render ZHeader myz non loggato", async () => {
+    const page = await newSpecPage({
+      components: [ZHeader],
+      html: initHeader(false, false, true, true, true)
+    });
+    expect(page.root).toEqualHtml(`
+    <z-header
+      intlinkdata='[{"id":"home","name":"Home","url":"#home","subMenu":[{"id":"libreria","name":"la mia libreria","url":"#libreria"}]},{"id":"Dizionari","name":"Dizionari","url":"https://www.zanichelli.it/dizionari/in-primo-piano"}]'
+      extlinkdata='[{"id":"supporto","name":"Supporto","url":"https://www.zanichelli.it/contatti-e-recapiti","icon":"question-mark.png"}]'
+      ismyz="true"
+    >
+      <mock:shadow-root>
+        <header>
+          <div class="top-header">
+            <div class="editors">
+              <slot name="editors" />
+            </div>
+          </div>
+          <div id="main-header" class="main-header false false">
+            <div class="logo">
+              <z-logo
+                link="https://www.zanichelli.it"
+                targetblank
+                imageurl="../../assets/images/png/zanichelli-logo-2.png"
+                imagealt="logo zanichelli"
+              />
+            </div>
+            <div></div>
+            <div></div>
+            <div class="login">
+              <z-button label="entra" type="secondary" buttonid="login-button"></z-button>
+            </div>
+          </div>
+          <div id="dropdown-menu" class="dropdown-menu hidden false"></div>
+        </header>
+      </mock:shadow-root>
+    </z-header>
+  `);
+  });
+
+  it("Test render ZHeader non myz loggato", async () => {
+    const page = await newSpecPage({
+      components: [ZHeader],
+      html: initHeader(true, true, false, true, true)
+    });
+    expect(page.root).toEqualHtml(`
+    <z-header
+      intlinkdata='[{"id":"home","name":"Home","url":"#home","subMenu":[{"id":"libreria","name":"la mia libreria","url":"#libreria"}]},{"id":"Dizionari","name":"Dizionari","url":"https://www.zanichelli.it/dizionari/in-primo-piano"}]'
+      extlinkdata='[{"id":"supporto","name":"Supporto","url":"https://www.zanichelli.it/contatti-e-recapiti","icon":"question-mark.png"}]'
+      userdata='{"islogged":true,"id":123456,"name":"Dario Docente e Professore","usergroup":15}'
+      ismyz="false"
+    >
+      <mock:shadow-root>
+        <header>
+          <div></div>
+          <div id="main-header" class="main-header myz-out false">
+            <div class="logo">
+              <z-logo
+                link="https://www.zanichelli.it"
+                targetblank
+                imageurl="../../assets/images/png/zanichelli-logo-2.png"
+                imagealt="logo zanichelli"
+              />
+            </div>
+            <div></div>
+            <div id="link-ext" class="link-ext">
+              <span class="link-ext-span false">
+                <z-link id="supporto" url="https://www.zanichelli.it/contatti-e-recapiti" label="Supporto" icon="question-mark.png" target="_blank" />
+              </span>
+            </div>
+            <div class="login">
+              <z-menu-dropdown
+                nomeutente="Dario Docente e Professore"
+                menucontent='[{"text":"Profilo", "link":"http://www.zanichelli.it"},{"text":"Esci", "link":"#home", "linkid":"logout-button"}]'
+              ></z-menu-dropdown>
+            </div>
+          </div>
+          <div id="dropdown-menu" class="dropdown-menu hidden false"></div>
+        </header>
+      </mock:shadow-root>
+    </z-header>
+  `);
+  });
+
+  it("Test render ZHeader active menu item", async () => {
+    const page = await newSpecPage({
+      components: [ZHeader],
+      html: initHeader(true, true, true, true, true)
+    });
+    page.rootInstance.activeMenuItem = {
+      id: "Dizionari",
+      name: "Dizionari",
+      url: "https://www.zanichelli.it/dizionari/in-primo-piano"
+    };
+    await page.waitForChanges();
+    expect(page.root).toEqualHtml(`
+    <z-header
+      intlinkdata='[{"id":"home","name":"Home","url":"#home","subMenu":[{"id":"libreria","name":"la mia libreria","url":"#libreria"}]},{"id":"Dizionari","name":"Dizionari","url":"https://www.zanichelli.it/dizionari/in-primo-piano"}]'
+      extlinkdata='[{"id":"supporto","name":"Supporto","url":"https://www.zanichelli.it/contatti-e-recapiti","icon":"question-mark.png"}]'
+      userdata='{"islogged":true,"id":123456,"name":"Dario Docente e Professore","usergroup":15}'
+      ismyz="true"
+    >
+      <mock:shadow-root>
+        <header>
+          <div class="top-header">
+            <div class="editors">
+              <slot name="editors" />
+            </div>
+          </div>
+          <div id="main-header" class="main-header false false">
+            <div class="logo">
+              <z-logo
+                link="https://www.zanichelli.it"
+                targetblank
+                imageurl="../../assets/images/png/zanichelli-logo-2.png"
+                imagealt="logo zanichelli"
+              />
+            </div>
+            <div id="link-int" class="link-int">
+              <span>
+                <a href="#home" id="home" class="menu-item">
+                  <span>Home</span>
+                  <i></i>
+                </a>
+                <svg height="8" width="16" class="hidden"><polygon points="8,0 16,8 0,8" class="arrow"></polygon></svg>
+                <span class="mobile-dropdown" id="mobile-dropdown-home">
+                  <z-list></z-list>
+                </span>
+              </span>
+              <span>
+                <a id="Dizionari" class="menu-item" href="https://www.zanichelli.it/dizionari/in-primo-piano">
+                  <span>Dizionari</span>
+                </a>
+                <svg height="8" width="16"><polygon points="8,0 16,8 0,8" class="arrow"></polygon></svg>
+              </span>
+            </div>
+            <div id="link-ext" class="link-ext">
+              <span class="link-ext-span myz">
+                <z-link id="supporto" url="https://www.zanichelli.it/contatti-e-recapiti" label="Supporto" icon="question-mark.png" iswhite target="_blank" />
+              </span>
+            </div>
+            <div class="login">
+              <z-menu-dropdown
+                nomeutente="Dario Docente e Professore"
+                menucontent='[{"text":"Profilo", "link":"http://www.zanichelli.it"},{"text":"Esci", "link":"#home", "linkid":"logout-button"}]'
+              ></z-menu-dropdown>
+            </div>
+          </div>
+          <div id="dropdown-menu" class="dropdown-menu hidden false"></div>
+        </header>
+      </mock:shadow-root>
+    </z-header>
+  `);
+  });
+
+  /*
+  TEST DESKTOP END
   */
 });
 
