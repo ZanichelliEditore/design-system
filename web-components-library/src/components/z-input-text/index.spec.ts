1bfda905cb0615757535a66b2f93edf549d2796c
Valentina Pasquino <valentina.pasquino@thinkopen.it>
2019-08-23 14:06:35 +0200

    MYZ-6915 test base z-input-text

--- web-components-library/src/components/z-input-text/index.spec.ts
+++ web-components-library/src/components/z-input-text/index.spec.ts
@@ -4,22 +4,36 @@ import { ZInputText } from "./index";
 
 describe("Suite test ZInputText", () => {
   it("Test render ZInputText vuoto", async () => {
-
     const page = await newSpecPage({
       components: [ZInputText],
       html: `<z-input-text></z-input-text>`
     });
-
     expect(page.root).toEqualHtml(`
       <z-input-text>
         <mock:shadow-root>
           <div>
-            This is your z-input-text component
-            This is your default prop:
+            <label></label>
+            <input />
           </div>
         </mock:shadow-root>
       </z-input-text>
-    `)
+    `);
+  });
+
+  it("Test render ZInputText con attributi", async () => {
+    const page = await newSpecPage({
+      components: [ZInputText],
+      html: `<z-input-text type='text' inputid='test' placeholder='placeholder' value='value' label='label'></z-input-text>`
+    });
+    expect(page.root).toEqualHtml(`
+      <z-input-text type='text' inputid='test' placeholder='placeholder' value='value' label='label'>
+        <mock:shadow-root>
+          <div>
+            <label>label</label>
+            <input type='text' id='test' name='test' placeholder='placeholder' value='value' />
+          </div>
+        </mock:shadow-root>
+      </z-input-text>
+    `);
   });
 });
-
