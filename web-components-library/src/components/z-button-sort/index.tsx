20cdfdd8f7913ff523845a63f61d37c6b6f23845
Valentina Pasquino <valentina.pasquino@thinkopen.it>
2019-09-02 17:45:16 +0200

    fix state

--- web-components-library/src/components/z-button-sort/index.tsx
+++ web-components-library/src/components/z-button-sort/index.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop, h, State, Event, EventEmitter } from '@stencil/core';
+import { Component, Prop, h, Event, EventEmitter } from '@stencil/core';
 
 @Component({
   tag: 'z-button-sort',
@@ -13,34 +13,26 @@ export class ZButtonSort {
   @Prop() counter?: number;
   @Prop() sortlabelasc?: string = 'A-Z';
   @Prop() sortlabeldesc?: string = 'Z-A';
-  @Prop() isselected?: boolean = false;
-  @Prop() sortasc?: boolean = true;
-
-  @State() isSelected: boolean;
-  @State() sortAsc: boolean;
-
-  componentWillLoad() {
-    this.isSelected = this.isselected;
-    this.sortAsc = this.sortasc;
-  }
+  @Prop({ mutable: true }) isselected?: boolean = false;
+  @Prop({ mutable: true }) sortasc?: boolean = true;
 
   @Event() buttonSortClick: EventEmitter;
   emitButtonSortClick() {
-    if (!this.isSelected) {
-      this.isSelected = !this.isSelected;
+    if (!this.isselected) {
+      this.isselected = true;
     } else {
-      this.sortAsc = !this.sortAsc;
+      this.sortasc = !this.sortasc;
     }
-    this.buttonSortClick.emit({ buttonid: this.buttonid, sortAsc: this.sortAsc });
+    this.buttonSortClick.emit({ buttonid: this.buttonid, sortAsc: this.sortasc });
   }
 
   render() {
     return (
-      <button id={this.buttonid} class={this.isSelected && "selected"} onClick={() => this.emitButtonSortClick()}>
+      <button id={this.buttonid} class={this.isselected && "selected"} onClick={() => this.emitButtonSortClick()}>
         <label>
-          {this.sortAsc ? this.label : this.desclabel}
+          {this.sortasc ? this.label : this.desclabel}
           {this.counter && ` (${this.counter})`}</label>
-        <span>{this.sortAsc ? this.sortlabelasc : this.sortlabeldesc}</span>
+        <span>{this.sortasc ? this.sortlabelasc : this.sortlabeldesc}</span>
         <z-icon name="ordina-icon-stroked" width={16} height={16} />
       </button>
     );
