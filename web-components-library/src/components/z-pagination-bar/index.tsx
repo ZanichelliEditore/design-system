063bc8ff0f46f445cf769f90839e97ebd90666e2
marco sirianni <marco.sirianni@thinkopen.it>
2019-10-14 12:09:01 +0200

    MYZ-6920 - Fix component paginationwith mobile swipe

--- web-components-library/src/components/z-pagination-bar/index.tsx
+++ web-components-library/src/components/z-pagination-bar/index.tsx
@@ -1,11 +1,10 @@
-import { Component, Prop, h, State, Event, EventEmitter } from '@stencil/core';
+import { Component, Prop, h, State, Event, EventEmitter } from "@stencil/core";
 
 @Component({
-  tag: 'z-pagination-bar',
-  styleUrl: 'styles.css',
+  tag: "z-pagination-bar",
+  styleUrl: "styles.css",
   shadow: true
 })
-
 export class ZPaginationBar {
   @Prop() pages: number;
   @Prop() visiblepages: number;
@@ -14,6 +13,11 @@ export class ZPaginationBar {
 
   @State() startpage: number = 1;
   @State() currentPages: number[] = [];
+
+  //Mobile swipe variables
+  private startTouch: number;
+  private startTouchPage: number;
+  private touchScrollSize: number = 60;
 
   @Event() goToPage: EventEmitter;
   emitGoToPage(page) {
@@ -41,40 +45,88 @@ export class ZPaginationBar {
   }
 
   pageWindow() {
-    return Math.min(this.pages, this.visiblepages)  //How many pages are there to show?
+    return Math.min(this.pages, this.visiblepages); //How many pages are there to show?
   }
 
   canNavigateLeft() {
-    return this.startpage > 1
+    return this.startpage > 1;
   }
   canNavigateRight() {
-    return (this.startpage + this.visiblepages - 1) < this.pages
+    return this.startpage + this.visiblepages - 1 < this.pages;
   }
 
   navigateLeft() {
     if (this.canNavigateLeft()) {
       this.startpage--;
-      this.loadPages()
+      this.loadPages();
     }
   }
 
   navigateRight() {
     if (this.canNavigateRight()) {
       this.startpage++;
-      this.loadPages()
+      this.loadPages();
+    }
+  }
+
+  touchStart(e: TouchEvent) {
+    e.preventDefault();
+    this.startTouch = e.changedTouches[0].clientX;
+    this.startTouchPage = this.startpage;
+  }
+
+  calculateTouchMovement(diffX: number) {
+    return Math.abs(Math.floor(diffX / this.touchScrollSize));
+  }
+
+  touchMove(e: TouchEvent) {
+    e.preventDefault();
+    const diffX = e.changedTouches[0].clientX - this.startTouch;
+    const movement = this.calculateTouchMovement(diffX);
+
+    if (diffX > 0) {
+      if (!this.canNavigateLeft()) return;
+
+      const deltaPage = Math.max(1, this.startTouchPage - movement);
+      if (deltaPage != this.startpage) this.startpage = deltaPage;
+    } else if (diffX < 0) {
+      if (!this.canNavigateRight()) return;
+
+      const deltaPage = Math.min(
+        this.startTouchPage + movement,
+        this.pages - this.visiblepages + 1
+      );
+      if (deltaPage != this.startpage) this.startpage = deltaPage;
     }
   }
 
   render() {
     return (
-      <div>
-        <z-icon name="chevron-left" class={!this.canNavigateLeft() && 'disabled'} onClick={() => this.navigateLeft()} />
-        {this.currentPages.map((page) => (
-          <z-pagination-page value={page} isselected={page === this.currentpage}
+      <div
+        onTouchStart={(e: TouchEvent) => {
+          this.touchStart(e);
+        }}
+        onTouchMove={(e: TouchEvent) => {
+          this.touchMove(e);
+        }}
+      >
+        <z-icon
+          name="chevron-left"
+          class={!this.canNavigateLeft() && "disabled"}
+          onClick={() => this.navigateLeft()}
+        />
+        {this.currentPages.map(page => (
+          <z-pagination-page
+            value={page}
+            isselected={page === this.currentpage}
             onClick={() => this.emitGoToPage(page)}
           />
         ))}
-        <z-icon name="chevron-right" class={!this.canNavigateRight() && 'disabled'} onClick={() => this.navigateRight()} />
+        <z-icon
+          name="chevron-right"
+          class={!this.canNavigateRight() && "disabled"}
+          onClick={() => this.navigateRight()}
+        />
       </div>
     );
   }
