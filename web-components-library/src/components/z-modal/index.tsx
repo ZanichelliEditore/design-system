c205b7b83a37775f7cc8b4c8177d9fa363ec5ec8
Valentina Pasquino <valentina.pasquino@thinkopen.it>
2019-10-22 15:54:12 +0200

    MYZ-6912 mobile style

--- web-components-library/src/components/z-modal/index.tsx
+++ web-components-library/src/components/z-modal/index.tsx
@@ -1,4 +1,5 @@
 import { Component, Prop, h, Event, EventEmitter } from '@stencil/core';
+import { mobileBreakpoint } from "../../constants/breakpoints";
 
 @Component({
   tag: 'z-modal',
@@ -10,10 +11,30 @@ export class ZModal {
   @Prop() modalid: string;
   @Prop() modaltitle?: string;
   @Prop() modalsubtitle?: string;
+  @Prop() closelabel: string = 'chiudi';
+
+  private isMobile: boolean;
+
+  componentWillLoad() {
+    this.isMobile = window.innerWidth <= mobileBreakpoint;
+  }
 
   @Event() modalClose: EventEmitter;
   emitModalClose() {
     this.modalClose.emit({ modalid: this.modalid });
+  }
+
+  renderCloseIcon() {
+    if (this.isMobile) {
+      return <z-link icon="close" label={this.closelabel.toUpperCase()} data-action="modalClose" data-modal={this.modalid} />
+    } else {
+      return (
+        <z-icon name="circle-cross-fill" width={24} height={24}
+          data-action="modalClose" data-modal={this.modalid}
+          onClick={() => this.emitModalClose()}
+        />
+      );
+    }
   }
 
   render() {
@@ -25,10 +46,7 @@ export class ZModal {
               <h1>{this.modaltitle}</h1>
               <h2>{this.modalsubtitle}</h2>
             </div>
-            <z-icon name="circle-cross-fill" width={24} height={24}
-              data-action="modalClose" data-modal={this.modalid}
-              onClick={() => this.emitModalClose()}
-            />
+            {this.renderCloseIcon()}
           </header>
           <main>
             <slot name="modalContent" />
