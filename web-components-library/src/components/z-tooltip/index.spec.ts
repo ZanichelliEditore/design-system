3978216a1e163f3c7584df09700374d69cc8bbb4
marco sirianni <marco.sirianni@thinkopen.it>
2019-11-04 10:32:35 +0100

    MYZ-6953 - Tooltip tests

--- web-components-library/src/components/z-tooltip/index.spec.ts
+++ web-components-library/src/components/z-tooltip/index.spec.ts
@@ -4,7 +4,6 @@ import { ZTooltip } from "./index";
 
 describe("Suite test ZTooltip", () => {
   it("Test render ZTooltip vuoto", async () => {
-
     const page = await newSpecPage({
       components: [ZTooltip],
       html: `<z-tooltip></z-tooltip>`
@@ -14,12 +13,77 @@ describe("Suite test ZTooltip", () => {
       <z-tooltip>
         <mock:shadow-root>
           <div>
-            This is your z-tooltip component
-            This is your default prop:
           </div>
         </mock:shadow-root>
       </z-tooltip>
-    `)
+    `);
+  });
+
+  it("Test render ZTooltip left", async () => {
+    const page = await newSpecPage({
+      components: [ZTooltip],
+      html: `<z-tooltip content='Ciao' type='left'></z-tooltip>`
+    });
+
+    expect(page.root).toEqualHtml(`
+      <z-tooltip content='Ciao' type='left'>
+        <mock:shadow-root>
+          <div class="tooltip tooltip-right-arrow">
+          Ciao
+          </div>
+        </mock:shadow-root>
+      </z-tooltip>
+    `);
+  });
+
+  it("Test render ZTooltip right", async () => {
+    const page = await newSpecPage({
+      components: [ZTooltip],
+      html: `<z-tooltip content='Ciao' type='right'></z-tooltip>`
+    });
+
+    expect(page.root).toEqualHtml(`
+      <z-tooltip content='Ciao' type='right'>
+        <mock:shadow-root>
+          <div class="tooltip tooltip-left-arrow">
+          Ciao
+          </div>
+        </mock:shadow-root>
+      </z-tooltip>
+    `);
+  });
+
+  it("Test render ZTooltip top", async () => {
+    const page = await newSpecPage({
+      components: [ZTooltip],
+      html: `<z-tooltip content='Ciao' type='top'></z-tooltip>`
+    });
+
+    expect(page.root).toEqualHtml(`
+      <z-tooltip content='Ciao' type='top'>
+        <mock:shadow-root>
+          <div class="tooltip tooltip-bottom-arrow">
+          Ciao
+          </div>
+        </mock:shadow-root>
+      </z-tooltip>
+    `);
+  });
+
+  it("Test render ZTooltip bottom", async () => {
+    const page = await newSpecPage({
+      components: [ZTooltip],
+      html: `<z-tooltip content='Ciao' type='bottom'></z-tooltip>`
+    });
+
+    expect(page.root).toEqualHtml(`
+      <z-tooltip content='Ciao' type='bottom'>
+        <mock:shadow-root>
+          <div class="tooltip tooltip-top-arrow">
+          Ciao
+          </div>
+        </mock:shadow-root>
+      </z-tooltip>
+    `);
   });
 });
-
