daf7eedd249a45e50269f724ce71cb35f91646af
Valentina Pasquino <valentina.pasquino@thinkopen.it>
2020-01-02 17:30:26 +0100

    MYZ-6978 fix tests

--- web-components-library/src/components/z-card-icon/index.spec.ts
+++ web-components-library/src/components/z-card-icon/index.spec.ts
@@ -12,11 +12,30 @@ describe("Suite test ZCardIcon", () => {
     expect(page.root).toEqualHtml(`
     <z-card-icon icon=\"icon\">
       <mock:shadow-root>
-        <span>
+        <button tabindex="1">
           <span>
-            <z-icon height=\"18\" name=\"icon\" width=\"18\"></z-icon>
+            <z-icon height=\"16\" name=\"icon\" width=\"16\"></z-icon>
           </span>
-        </span>
+        </button>
+      </mock:shadow-root>
+    </z-card-icon>
+    `);
+  });
+
+  it("Test render ZCardIcon disabled", async () => {
+    const page = await newSpecPage({
+      components: [ZCardIcon],
+      html: `<z-card-icon icon="icon" isdisabled></z-card-icon>`
+    });
+
+    expect(page.root).toEqualHtml(`
+    <z-card-icon icon=\"icon\" isdisabled>
+      <mock:shadow-root>
+        <button tabindex="1" disabled>
+          <span>
+            <z-icon height=\"16\" name=\"icon\" width=\"16\"></z-icon>
+          </span>
+        </button>
       </mock:shadow-root>
     </z-card-icon>
     `);
