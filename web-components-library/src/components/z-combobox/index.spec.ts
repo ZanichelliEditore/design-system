e06cf3c37405dfd8dea2190bc854b0dbfe9b4a33
aGambella <alice.gambella@absontheweb.com>
2020-02-14 12:25:37 +0100

    MYZ-6987 modifica test zCombobox per le modifiche apportate al componente

--- web-components-library/src/components/z-combobox/index.spec.ts
+++ web-components-library/src/components/z-combobox/index.spec.ts
@@ -156,6 +156,36 @@ describe("Suite test ZCombobox", () => {
   it("Test render ZCombobox aperta con ricerca attiva e risultati", async () => {
     const page = await newSpecPage({
       components: [ZCombobox],
+      html: `<z-combobox inputid="combo" label="combo" hassearch=true searchlabel="cerca" searchplaceholder="placeholder" closesearchtext="CHIUDI" items='[{"id":"item_1","name":"primo elemento","checked":false},{"id":"item_2","name":"secondo elemento","checked":true}]'></z-combobox>`
+    });
+    page.rootInstance.searchValue = "primo";
+    await page.waitForChanges();
+    expect(page.root).toEqualHtml(`
+      <z-combobox inputid="combo" label="combo" hassearch=true searchlabel="cerca" searchplaceholder="placeholder" closesearchtext="CHIUDI" items='[{"id":"item_1","name":"primo elemento","checked":false},{"id":"item_2","name":"secondo elemento","checked":true}]'>
+        <mock:shadow-root>
+          <div class="open false" data-action="combo-combo" id="combo">
+            <header tabindex="0">
+              <h2>combo<span>(1)</span></h2>
+              <z-icon name="drop-down" width="18" height="18" />
+            </header>
+            <div class="openComboData">
+              <z-input-text inputid="combo_search" label="cerca" placeholder="placeholder" type="text" value="primo"></z-input-text>
+              <div class="search">
+                <ul>
+                  <z-list-item id="item_1" text="<b>primo</b> elemento" listitemid="item_1" icon="checkbox-unchecked" action="combo-li-combo" tabindex="0"></z-list-item>
+                </ul>
+                <a role="button">CHIUDI</a>
+              </div>
+            </div>
+          </div>
+        </mock:shadow-root>
+      </z-combobox>
+    `);
+  });
+
+  it("Test render ZCombobox aperta con ricerca attiva senza testo di chiusura della ricerca testuale", async () => {
+    const page = await newSpecPage({
+      components: [ZCombobox],
       html: `<z-combobox inputid="combo" label="combo" hassearch=true searchlabel="cerca" searchplaceholder="placeholder" items='[{"id":"item_1","name":"primo elemento","checked":false},{"id":"item_2","name":"secondo elemento","checked":true}]'></z-combobox>`
     });
     page.rootInstance.searchValue = "primo";
@@ -174,7 +204,7 @@ describe("Suite test ZCombobox", () => {
                 <ul>
                   <z-list-item id="item_1" text="<b>primo</b> elemento" listitemid="item_1" icon="checkbox-unchecked" action="combo-li-combo" tabindex="0"></z-list-item>
                 </ul>
-                <a>CHIUDI</a>
+                <a role="button"></a>
               </div>
             </div>
           </div>
@@ -186,12 +216,12 @@ describe("Suite test ZCombobox", () => {
   it("Test render ZCombobox aperta con ricerca attiva senza risultati", async () => {
     const page = await newSpecPage({
       components: [ZCombobox],
-      html: `<z-combobox inputid="combo" label="combo" hassearch=true searchlabel="cerca" searchplaceholder="placeholder" items='[{"id":"item_1","name":"primo elemento","checked":false},{"id":"item_2","name":"secondo elemento","checked":true}]' noresultslabel='non ci sono risultati'></z-combobox>`
+      html: `<z-combobox inputid="combo" label="combo" hassearch=true searchlabel="cerca" searchplaceholder="placeholder" closesearchtext="CHIUDI" items='[{"id":"item_1","name":"primo elemento","checked":false},{"id":"item_2","name":"secondo elemento","checked":true}]' noresultslabel='non ci sono risultati'></z-combobox>`
     });
     page.rootInstance.searchValue = "prova";
     await page.waitForChanges();
     expect(page.root).toEqualHtml(`
-      <z-combobox inputid="combo" label="combo" hassearch=true searchlabel="cerca" searchplaceholder="placeholder" items='[{"id":"item_1","name":"primo elemento","checked":false},{"id":"item_2","name":"secondo elemento","checked":true}]' noresultslabel='non ci sono risultati'>
+      <z-combobox inputid="combo" label="combo" hassearch=true searchlabel="cerca" searchplaceholder="placeholder" closesearchtext="CHIUDI" items='[{"id":"item_1","name":"primo elemento","checked":false},{"id":"item_2","name":"secondo elemento","checked":true}]' noresultslabel='non ci sono risultati'>
         <mock:shadow-root>
           <div class="open false" data-action="combo-combo" id="combo">
             <header tabindex="0">
@@ -204,7 +234,7 @@ describe("Suite test ZCombobox", () => {
                 <ul>
                   <z-list-item id='no-results' text='non ci sono risultati' listitemid='no-results' icon='circle-cross-stroke'></z-list-item>
                 </ul>
-                <a>CHIUDI</a>
+                <a role="button">CHIUDI</a>
               </div>
             </div>
           </div>
