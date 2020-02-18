31401ce82f5b6b2361df83d6763006c7a912ee3e
Valentina Pasquino <valentina.pasquino@thinkopen.it>
2020-02-18 11:39:39 +0100

    added group to readme generator

--- web-components-library/internals/scripts/build-docs.sh
+++ web-components-library/internals/scripts/build-docs.sh
@@ -3,9 +3,14 @@
 echo "generating global readme docs"
 
 dir="src/components"
-readme_file=$dir"/readme.md"
+readme_dir="src/readme"
 
 readme=""
+
+IFS='"'
+
+eval "$(rm -R ${readme_dir})"
+eval "$(mkdir -m775 ${readme_dir})"
 
 for subdir in "$dir"/*
 do
@@ -13,9 +18,18 @@ do
   do
     file="$(basename -- $doc)"
     if [ "$file" = "readme.md" ]; then
-      readme="$readme""$(cat $doc)""\n\n---\n---\n\n"
+      group="$(cat $doc | grep 'readme-group')"
+      if [ -n "$group" ]; then
+        read -ra arr <<< "$group"
+        readme_file="readme-${arr[1]}.md"
+      else
+        readme_file="readme.md"
+      fi
+      readme="$(cat $doc)"
+      echo "$readme" >> "$readme_dir/$readme_file"
+      echo $'\r---\r---' >> "$readme_dir/$readme_file"
     fi
   done
 done
 
-echo "$readme" >> $readme_file
+IFS=' '
