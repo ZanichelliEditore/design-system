40096eebbaec1436d23ae22b225ac0e97f8be30d
pasquale <pasquale.merolle@thinkopen.it>
2020-06-09 14:51:02 +0200

    storybook scaffolding

--- .storybook/folders.js
+++ .storybook/folders.js
@@ -1,4 +1,14 @@
 export const folders = {
   Buttons : 'Buttons',
-  Footer: 'Footer'
+  Card: 'Card',
+  Footer: 'Footer',
+  Icons: 'Icons',
+  Inputs: 'Inputs',
+  List: 'List',
+  Logo: 'Logo',
+  Modal: 'Modal',
+  Navigation: 'Navigation',
+  Notification: 'Notification',
+  Pagination: 'Pagination',
+  Panel: 'Panel'
 }
