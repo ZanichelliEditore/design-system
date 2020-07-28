fb95a6d7b640ffdddb2b84ccfe3062d3c9daa132
Michele Salvador <michele.salvador@absontheweb.com>
2020-07-28 15:47:30 +0200

    MYZ-7055 built some tests

--- src/components/inputs/z-otp/index.spec.ts
+++ src/components/inputs/z-otp/index.spec.ts
@@ -3,8 +3,7 @@ import { newSpecPage } from "@stencil/core/testing";
 import { ZOtp } from "./index";
 
 describe("Suite test ZOtp", () => {
-  it("Test render ZOtp vuoto", async () => {
-
+  it("Test render ZOtp senza prop", async () => {
     const page = await newSpecPage({
       components: [ZOtp],
       html: `<z-otp></z-otp>`
@@ -14,12 +13,34 @@ describe("Suite test ZOtp", () => {
       <z-otp>
         <mock:shadow-root>
           <div>
-            This is your z-otp component
-            This is your default prop:
+            <input type="tel" autocomplete="off" maxlength="1">
+            <input type="tel" autocomplete="off" maxlength="1">
+            <input type="tel" autocomplete="off" maxlength="1">
+            <input type="tel" autocomplete="off" maxlength="1">
+            <input type="tel" autocomplete="off" maxlength="1">
+            <input type="tel" autocomplete="off" maxlength="1">
           </div>
         </mock:shadow-root>
       </z-otp>
-    `)
+    `);
+  });
+
+  it("Test render ZOtp con prop inputNum", async () => {
+    const page = await newSpecPage({
+      components: [ZOtp],
+      html: `<z-otp inputNum='3'></z-otp>`
+    });
+
+    expect(page.root).toEqualHtml(`
+      <z-otp>
+        <mock:shadow-root>
+          <div>
+            <input type="tel" autocomplete="off" maxlength="1">
+            <input type="tel" autocomplete="off" maxlength="1">
+            <input type="tel" autocomplete="off" maxlength="1">
+          </div>
+        </mock:shadow-root>
+      </z-otp>
+    `);
   });
 });
-
