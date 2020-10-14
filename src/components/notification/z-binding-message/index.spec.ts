2e7827eeeeb07eab3ca3f5d934f986b51ede0e96
ufollettu <pasquale.merolle@thinkopen.it>
2020-10-14 21:03:48 +0200

    feat: z-binding-message storybook and test

--- src/components/notification/z-binding-message/index.spec.ts
+++ src/components/notification/z-binding-message/index.spec.ts
@@ -3,114 +3,109 @@ import { newSpecPage } from "@stencil/core/testing";
 import { ZBindingMessage } from "./index";
 
 describe("Suite test ZBindingMessage", () => {
-  it("Test render ZBindingMessage base", async () => {
+  it("Test render empty ZBindingMessage", async () => {
 
     const page = await newSpecPage({
       components: [ZBindingMessage],
-      html: `  <z-binding-message
-                  bartitle="Su questo sito usiamo i cookie."
-                  preventcookieset="true"
-                  cookiepolicyurl="https://my.zanichelli.it/privacy#Cookie"
-                  hide="false"></z-binding-message>`
+      html: `  <z-binding-message messagetitle="Su questo sito usiamo i cookie."></z-binding-message>`
     });
 
     expect(page.root).toEqualHtml(`
-    <z-binding-message
-      bartitle="Su questo sito usiamo i cookie."
-      preventcookieset="true"
-      cookiepolicyurl="https://my.zanichelli.it/privacy#Cookie"
-      hide="false">
-        <mock:shadow-root>
-          <div class="">
-            <div class="wrapper">
-              <div class="content">
-                <div>
-                  <h2>Su questo sito usiamo i cookie.</h2>
-                  <div>
-                    <p>Se continui a navigare ci permetti di farlo secondo le regole spiegate nella nostra informativa sulla privacy relativa ai &nbsp;
-                      <a href="https://my.zanichelli.it/privacy#Cookie" target="_blank">cookie</a>.
-                    </p>
-                  </div>
-                </div>
-                <z-button variant="primary">OK</z-button>
-              </div>
+    <z-binding-message messagetitle="Su questo sito usiamo i cookie.">
+      <mock:shadow-root>
+        <div>
+          <div>
+            <h2>
+              Su questo sito usiamo i cookie.
+            </h2>
+            <div>
+              <slot name="description"></slot>
             </div>
           </div>
-        </mock:shadow-root>
-      </z-binding-message>
+          <slot name="cta"></slot>
+        </div>
+      </mock:shadow-root>
+    </z-binding-message>
     `)
   });
 
-  it("Test render ZBindingMessage senza titolo", async () => {
+  it("Test render ZBindingMessage with description", async () => {
 
     const page = await newSpecPage({
       components: [ZBindingMessage],
-      html: `  <z-binding-message
-                  preventcookieset="true"
-                  cookiepolicyurl="https://my.zanichelli.it/privacy#Cookie"
-                  hide="false"></z-binding-message>`
+      html: `
+      <z-binding-message messagetitle="Su questo sito usiamo i cookie.">
+        <p slot="description">Se continui a navigare ci permetti di farlo secondo le regole spiegate nella nostra informativa sulla privacy relativa ai &nbsp;
+          <a href="https://my.zanichelli.it/privacy#Cookie" target="_blank">cookie</a>.
+        </p>
+      </z-binding-message>
+      `
     });
 
     expect(page.root).toEqualHtml(`
-    <z-binding-message
-      preventcookieset="true"
-      cookiepolicyurl="https://my.zanichelli.it/privacy#Cookie"
-      hide="false">
-        <mock:shadow-root>
-          <div class="">
-            <div class="wrapper">
-              <div class="content">
-                <div>
-                  <h2></h2>
-                  <div>
-                    <p>Se continui a navigare ci permetti di farlo secondo le regole spiegate nella nostra informativa sulla privacy relativa ai &nbsp;
-                      <a href="https://my.zanichelli.it/privacy#Cookie" target="_blank">cookie</a>.
-                    </p>
-                  </div>
-                </div>
-                <z-button variant="primary">OK</z-button>
-              </div>
+    <z-binding-message messagetitle="Su questo sito usiamo i cookie.">
+      <mock:shadow-root>
+        <div>
+          <div>
+            <h2>
+              Su questo sito usiamo i cookie.
+            </h2>
+            <div>
+              <slot name="description"></slot>
             </div>
           </div>
-        </mock:shadow-root>
-      </z-binding-message>
+          <slot name="cta"></slot>
+        </div>
+      </mock:shadow-root>
+      <p slot="description">
+        Se continui a navigare ci permetti di farlo secondo le regole spiegate nella nostra informativa sulla privacy relativa ai
+        <a href="https://my.zanichelli.it/privacy#Cookie" target="_blank">
+          cookie
+        </a>
+        .
+      </p>
+    </z-binding-message>
     `)
   });
 
-  it("Test render ZBindingMessage nascosto", async () => {
-
+  it("Test render empty ZBindingMessage with description and cta", async () => {
     const page = await newSpecPage({
       components: [ZBindingMessage],
-      html: `  <z-binding-message
-                  preventcookieset="true"
-                  cookiepolicyurl="https://my.zanichelli.it/privacy#Cookie"
-                  hide="true"></z-binding-message>`
+      html: `
+      <z-binding-message messagetitle="Su questo sito usiamo i cookie.">
+        <p slot="description">Se continui a navigare ci permetti di farlo secondo le regole spiegate nella nostra informativa sulla privacy relativa ai &nbsp;
+          <a href="https://my.zanichelli.it/privacy#Cookie" target="_blank">cookie</a>.
+        </p>
+      <z-button slot="cta" variant="primary">OK</z-button>
+      </z-binding-message>`
     });
 
     expect(page.root).toEqualHtml(`
-    <z-binding-message
-      preventcookieset="true"
-      cookiepolicyurl="https://my.zanichelli.it/privacy#Cookie"
-      hide="true">
-        <mock:shadow-root>
-          <div class="hidden">
-            <div class="wrapper">
-              <div class="content">
-                <div>
-                  <h2></h2>
-                  <div>
-                    <p>Se continui a navigare ci permetti di farlo secondo le regole spiegate nella nostra informativa sulla privacy relativa ai &nbsp;
-                      <a href="https://my.zanichelli.it/privacy#Cookie" target="_blank">cookie</a>.
-                    </p>
-                  </div>
-                </div>
-                <z-button variant="primary">OK</z-button>
-              </div>
+    <z-binding-message messagetitle="Su questo sito usiamo i cookie.">
+      <mock:shadow-root>
+        <div>
+          <div>
+            <h2>
+              Su questo sito usiamo i cookie.
+            </h2>
+            <div>
+              <slot name="description"></slot>
             </div>
           </div>
-        </mock:shadow-root>
-      </z-binding-message>
-    `)
+          <slot name="cta"></slot>
+        </div>
+      </mock:shadow-root>
+      <p slot="description">
+        Se continui a navigare ci permetti di farlo secondo le regole spiegate nella nostra informativa sulla privacy relativa ai
+        <a href="https://my.zanichelli.it/privacy#Cookie" target="_blank">
+          cookie
+        </a>
+        .
+      </p>
+      <z-button slot="cta" variant="primary">
+        OK
+      </z-button>
+    </z-binding-message>
+    `);
   });
 });
-
--- src/components/notification/z-binding-message/readme.md
+++ src/components/notification/z-binding-message/readme.md
@@ -7,27 +7,10 @@
 
 ## Properties
 
-| Property           | Attribute          | Description                                                                      | Type      | Default     |
-| ------------------ | ------------------ | -------------------------------------------------------------------------------- | --------- | ----------- |
-| `bartitle`         | `bartitle`         | title cookiebar text                                                             | `string`  | `undefined` |
-| `cookiepolicyurl`  | `cookiepolicyurl`  | cookie policy link url                                                           | `string`  | `undefined` |
-| `hide`             | `hide`             | hide cookie bar (optional)                                                       | `boolean` | `undefined` |
-| `preventcookieset` | `preventcookieset` | prevent default cookie set action on 'OK' button click (optional). default false | `boolean` | `undefined` |
+| Property       | Attribute      | Description   | Type     | Default     |
+| -------------- | -------------- | ------------- | -------- | ----------- |
+| `messagetitle` | `messagetitle` | message title | `string` | `undefined` |
 
-
-## Dependencies
-
-### Depends on
-
-- [z-button](../../buttons/z-button)
-
-### Graph
-```mermaid
-graph TD;
-  z-cookiebar --> z-button
-  z-button --> z-icon
-  style z-cookiebar fill:#f9f,stroke:#333,stroke-width:4px
-```
 
 ----------------------------------------------
 
