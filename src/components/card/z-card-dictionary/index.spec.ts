4bdf32a6ca4629fdbb9c7a6a86233a5828a8ef57
Valentina Pasquino <valentina.pasquino@thinkopen.it>
2020-12-14 17:41:07 +0100

    feat: MYZ-7499 tests and stories

--- src/components/card/z-card-dictionary/index.spec.ts
+++ src/components/card/z-card-dictionary/index.spec.ts
@@ -36,10 +36,40 @@ describe("Suite test ZCardDictionary", () => {
   it("Test render ZCardDictionary - with props", async () => {
     const page = await newSpecPage({
       components: [ZCardDictionary],
-      html: `<z-card-dictionary name="title" cover="img-url" flipped="true" disabled="true"></z-card-dictionary>`
+      html: `<z-card-dictionary name="title" cover="img-url" disabled="true"></z-card-dictionary>`
     });
     expect(page.root).toEqualHtml(`
-      <z-card-dictionary name="title" cover="img-url" flipped="true" disabled="true">
+      <z-card-dictionary name="title" cover="img-url" disabled="true">
+        <mock:shadow-root>
+          <div>
+            <z-card>
+              <z-card-header titolo="title"></z-card-header>
+              <div class="content">
+                <div class="front">
+                  <z-card-body>
+                    <z-card-cover titolo="title" img="img-url" slot="cover" faded=""></z-card-cover>
+                  </z-card-body>
+                  <z-button variant="secondary" icon="informationsource" issmall="">INFO</z-button>
+                </div>
+                <div class="back">
+                  <slot name="info"></slot>
+                </div>
+              </div>
+              <slot></slot>
+            </z-card>
+          </div>
+        </mock:shadow-root>
+      </z-card-dictionary>
+    `);
+  });
+
+  it("Test render ZCardDictionary - flipped", async () => {
+    const page = await newSpecPage({
+      components: [ZCardDictionary],
+      html: `<z-card-dictionary name="title" cover="img-url" flipped="true"></z-card-dictionary>`
+    });
+    expect(page.root).toEqualHtml(`
+      <z-card-dictionary name="title" cover="img-url" flipped="true">
         <mock:shadow-root>
           <div>
             <z-card>
@@ -47,7 +77,7 @@ describe("Suite test ZCardDictionary", () => {
               <div class="content flipped">
                 <div class="front">
                   <z-card-body>
-                    <z-card-cover titolo="title" img="img-url" slot="cover" faded=""></z-card-cover>
+                    <z-card-cover titolo="title" img="img-url" slot="cover"></z-card-cover>
                   </z-card-body>
                   <z-button variant="secondary" icon="informationsource" issmall="">INFO</z-button>
                 </div>
