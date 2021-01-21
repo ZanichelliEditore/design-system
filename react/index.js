7837b8d76d5a4462d1a6a7867acad58cb192a5b9
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2021-01-21 16:22:33 +0100

    fix: use commonjs exports for react

--- react/index.js
+++ react/index.js
@@ -1,1 +1,22 @@
-export * from './dist/index.js';
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true,
+});
+
+var library = require('./dist/index.js');
+
+Object.keys(library).forEach(function (key) {
+  if (key === 'default' || key === '__esModule') {
+    return;
+  }
+  if (key in exports && exports[key] === library[key]) {
+    return;
+  }
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function () {
+      return library[key];
+    },
+  });
+});
