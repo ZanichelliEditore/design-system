f9ca1faf6337a4f170d125fe5cf8544cdfacc514
Paolo F. Marino <p.marino@esis-italia.com>
2021-04-01 16:25:09 +0200

    refactor: styles z-footer-link

--- src/components/footer/z-footer-link/index.spec.ts
+++ src/components/footer/z-footer-link/index.spec.ts
@@ -4,22 +4,17 @@ import { ZFooterLink } from "./index";
 
 describe("Suite test ZFooterLink", () => {
   it("Test render ZFooterLink vuoto", async () => {
-
     const page = await newSpecPage({
       components: [ZFooterLink],
-      html: `<z-footer-link></z-footer-link>`
+      html: `<z-footer-link></z-footer-link>`,
     });
 
     expect(page.root).toEqualHtml(`
       <z-footer-link>
         <mock:shadow-root>
-          <div>
-            This is your z-footer-link component
-            This is your default prop:
-          </div>
+          <a target="_blank"></a>
         </mock:shadow-root>
       </z-footer-link>
-    `)
+    `);
   });
 });
-
