a14e2cead771699451ad4600c72a50454830c5b0
pasquale <pasquale.merolle@thinkopen.it>
2021-04-10 23:29:11 +0200

    feat: handle tablet resize

--- src/components/z-topbar/index.tsx
+++ src/components/z-topbar/index.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop, h } from "@stencil/core";
+import { Component, Prop, Element, h, State } from "@stencil/core";
 
 @Component({
   tag: "z-topbar",
@@ -7,8 +7,37 @@ import { Component, Prop, h } from "@stencil/core";
 })
 export class ZTopbar {
   @Prop() hashtag: string;
+  @Element() hostElement: HTMLElement;
+
+  @State() zLinksValues: string[]
+
+  componentWillLoad() {
+    window.addEventListener("resize", () => {
+      this.checkIfMobile();
+    });
+    this.zLinksValues = Array.from(this.hostElement.children).filter(child => child.nodeName === 'Z-LINK').map(link => link.childNodes[0].nodeValue)
+    this.checkIfMobile();
+  }
+
+  checkIfMobile() {
+    if (this.hostElement) {
+
+      const zLinks = Array.from(this.hostElement.children).filter(child => child.nodeName === 'Z-LINK')
+      if (window.innerWidth <= 767) {
+        zLinks.forEach(link => {
+          link.childNodes[0].nodeValue = ''
+        })
+      } else {
+        zLinks.forEach((link, i) => {
+          link.childNodes[0].nodeValue = this.zLinksValues[i]
+        })
+      }
+    }
+  }
 
   render() {
+
+
     return (
       <div>
         <div class="left">
