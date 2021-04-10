3e4f58d195c62bf994e356d9086525197e9bc2c1
pasquale <pasquale.merolle@thinkopen.it>
2021-04-10 23:29:21 +0200

    feat: handle tablet resize

--- src/components/z-topbar/index.tsx
+++ src/components/z-topbar/index.tsx
@@ -9,35 +9,36 @@ export class ZTopbar {
   @Prop() hashtag: string;
   @Element() hostElement: HTMLElement;
 
-  @State() zLinksValues: string[]
+  @State() zLinksValues: string[];
 
   componentWillLoad() {
     window.addEventListener("resize", () => {
       this.checkIfMobile();
     });
-    this.zLinksValues = Array.from(this.hostElement.children).filter(child => child.nodeName === 'Z-LINK').map(link => link.childNodes[0].nodeValue)
+    this.zLinksValues = Array.from(this.hostElement.children)
+      .filter((child) => child.nodeName === "Z-LINK")
+      .map((link) => link.childNodes[0].nodeValue);
     this.checkIfMobile();
   }
 
   checkIfMobile() {
     if (this.hostElement) {
-
-      const zLinks = Array.from(this.hostElement.children).filter(child => child.nodeName === 'Z-LINK')
+      const zLinks = Array.from(this.hostElement.children).filter(
+        (child) => child.nodeName === "Z-LINK"
+      );
       if (window.innerWidth <= 767) {
-        zLinks.forEach(link => {
-          link.childNodes[0].nodeValue = ''
-        })
+        zLinks.forEach((link) => {
+          link.childNodes[0].nodeValue = "";
+        });
       } else {
         zLinks.forEach((link, i) => {
-          link.childNodes[0].nodeValue = this.zLinksValues[i]
-        })
+          link.childNodes[0].nodeValue = this.zLinksValues[i];
+        });
       }
     }
   }
 
   render() {
-
-
     return (
       <div>
         <div class="left">
