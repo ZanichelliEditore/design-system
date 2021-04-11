eea2d45635a277d4352e8db264f7bf9a51d2c03b
pasquale <pasquale.merolle@thinkopen.it>
2021-04-12 00:00:21 +0200

    feat: edited html and z-app-menu unit test

--- src/components/z-app-menu/index.spec.ts
+++ src/components/z-app-menu/index.spec.ts
@@ -3,7 +3,7 @@ import { newSpecPage } from "@stencil/core/testing";
 import { ZAppMenu } from "./index";
 
 describe("Suite test ZAppMenu", () => {
-  it("Test render ZAppMenu vuoto", async () => {
+  it("Test render ZAppMenu default", async () => {
 
     const page = await newSpecPage({
       components: [ZAppMenu],
@@ -13,10 +13,68 @@ describe("Suite test ZAppMenu", () => {
     expect(page.root).toEqualHtml(`
       <z-app-menu>
         <mock:shadow-root>
-          <div>
-            This is your z-app-menu component
-            This is your default prop:
-          </div>
+          <button>
+            <span class="dark"></span>
+            <span class="dark"></span>
+            <span class="dark"></span>
+            <span class="dark"></span>
+            <span class="dark"></span>
+            <span class="dark"></span>
+            <span class="dark"></span>
+            <span class="dark"></span>
+            <span class="dark"></span>
+          </button>
+        </mock:shadow-root>
+      </z-app-menu>
+    `)
+  });
+
+  it("Test render ZAppMenu dark", async () => {
+
+    const page = await newSpecPage({
+      components: [ZAppMenu],
+      html: `<z-app-menu variant="dark"></z-app-menu>`
+    });
+
+    expect(page.root).toEqualHtml(`
+      <z-app-menu variant="dark">
+        <mock:shadow-root>
+          <button>
+            <span class="dark"></span>
+            <span class="dark"></span>
+            <span class="dark"></span>
+            <span class="dark"></span>
+            <span class="dark"></span>
+            <span class="dark"></span>
+            <span class="dark"></span>
+            <span class="dark"></span>
+            <span class="dark"></span>
+          </button>
+        </mock:shadow-root>
+      </z-app-menu>
+    `)
+  });
+  it("Test render ZAppMenu light", async () => {
+
+    const page = await newSpecPage({
+      components: [ZAppMenu],
+      html: `<z-app-menu variant="light"></z-app-menu>`
+    });
+
+    expect(page.root).toEqualHtml(`
+      <z-app-menu variant="light">
+        <mock:shadow-root>
+          <button>
+            <span class="light"></span>
+            <span class="light"></span>
+            <span class="light"></span>
+            <span class="light"></span>
+            <span class="light"></span>
+            <span class="light"></span>
+            <span class="light"></span>
+            <span class="light"></span>
+            <span class="light"></span>
+          </button>
         </mock:shadow-root>
       </z-app-menu>
     `)
--- src/components/z-user-dropdown/index.spec.ts
+++ src/components/z-user-dropdown/index.spec.ts
@@ -3,7 +3,7 @@ import { newSpecPage } from "@stencil/core/testing";
 import { ZUserDropdown } from "./index";
 
 describe("Suite test ZUserDropdown", () => {
-  it("Test render ZUserDropdown vuoto", async () => {
+  it("Test render ZUserDropdown default", async () => {
 
     const page = await newSpecPage({
       components: [ZUserDropdown],
@@ -13,10 +13,85 @@ describe("Suite test ZUserDropdown", () => {
     expect(page.root).toEqualHtml(`
       <z-user-dropdown>
         <mock:shadow-root>
-          <div>
-            This is your z-user-dropdown component
-            This is your default prop:
-          </div>
+            <span>
+              <z-link big="" icon="enter" textcolor="white">
+                Entra
+              </z-link>
+            </span>
+        </mock:shadow-root>
+      </z-user-dropdown>
+    `)
+  });
+
+  it("Test render ZUserDropdown not logged", async () => {
+
+    const page = await newSpecPage({
+      components: [ZUserDropdown],
+      html: `<z-user-dropdown
+              userfullname="docente di calabresi per italiani"
+              islogged="false"
+              slot="login"
+            ></z-user-dropdown>`
+    });
+
+    expect(page.root).toEqualHtml(`
+      <z-user-dropdown>
+        <mock:shadow-root>
+            <span>
+              <z-link big="" icon="enter" textcolor="white">
+                Entra
+              </z-link>
+            </span>
+        </mock:shadow-root>
+      </z-user-dropdown>
+    `)
+  });
+
+  it("Test render ZUserDropdown logged", async () => {
+
+    const page = await newSpecPage({
+      components: [ZUserDropdown],
+      html: `<z-user-dropdown
+              userfullname="docente di italiano per calabresi"
+              islogged="true"
+              slot="login"
+              menucontent='[{"label":"Profilo", "link":"http://www.zanichelli.it", "icon":"arrow-quad-north-east"}, {"label":"I Tuoi Ordini", "link":"http://www.zanichelli.it", "icon":"exit"},{"label":"Esci", "link":"http://www.google.it", "icon":"enter"}]'
+            ></z-user-dropdown>`
+    });
+
+    expect(page.root).toEqualHtml(`
+      <z-user-dropdown>
+        <mock:shadow-root>
+            <span>
+              <z-link big="" icon="enter" textcolor="white">
+                Entra
+              </z-link>
+            </span>
+        </mock:shadow-root>
+      </z-user-dropdown>
+    `)
+  });
+  it("Test render ZUserDropdown logged", async () => {
+
+    const page = await newSpecPage({
+      components: [ZUserDropdown],
+      html: `<z-user-dropdown
+              variant="light"
+              userfullname="docente di italiano per calabresi"
+              islogged="true"
+              slot="login"
+              menucontent='[{"label":"Profilo", "link":"http://www.zanichelli.it", "icon":"arrow-quad-north-east"}, {"label":"I Tuoi Ordini", "link":"http://www.zanichelli.it", "icon":"exit"},{"label":"Esci", "link":"http://www.google.it", "icon":"enter"}]'
+            ></z-user-dropdown>`
+    });
+
+    expect(page.root).toEqualHtml(`
+      <z-user-dropdown>
+        <mock:shadow-root>
+            <span>
+              <z-link big="" icon="enter" textcolor="white">
+                Entra
+              </z-link>
+            </span>
         </mock:shadow-root>
       </z-user-dropdown>
     `)
