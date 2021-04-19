8a9c39fd4a0155bf625dcfbaaa81d584901f9243
Andrea De Castri <adecastri@zanichelli.it>
2021-04-19 16:41:47 +0200

    docs(clav-33): update docs for z-registro-table

--- src/snowflakes/registro-table/z-registro-table/index.spec.ts
+++ src/snowflakes/registro-table/z-registro-table/index.spec.ts
@@ -4,22 +4,27 @@ import { ZRegistroTable } from "./index";
 
 describe("Suite test ZRegistroTable", () => {
   it("Test render ZRegistroTable vuoto", async () => {
-
     const page = await newSpecPage({
       components: [ZRegistroTable],
-      html: `<z-registro-table></z-registro-table>`
+      html: `<z-registro-table></z-registro-table>`,
     });
 
     expect(page.root).toEqualHtml(`
       <z-registro-table>
         <mock:shadow-root>
-          <div>
-            This is your z-registro-table component
-            This is your default prop:
-          </div>
+          <table>
+            <thead>
+              <slot name=\"header\"></slot>
+            </thead>
+            <tbody>
+              <slot name=\"body\"></slot>
+            </tbody>
+            <tfoot>
+              <slot name=\"footer\"></slot>
+            </tfoot>
+          </table>
         </mock:shadow-root>
       </z-registro-table>
-    `)
+    `);
   });
 });
-
