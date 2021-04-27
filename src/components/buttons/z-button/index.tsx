be47e92686ffb998bc5bf16e4f87557e4ca19253
nicolo.carpignoli <nicolo.carpignoli@chialab.it>
2021-04-27 16:59:27 +0200

    refactored z-button

--- src/components/buttons/z-button/index.tsx
+++ src/components/buttons/z-button/index.tsx
@@ -2,7 +2,7 @@ import { Component, Prop, h, Element } from "@stencil/core";
 import {
   ButtonVariantBean,
   ButtonVariantEnum,
-  ButtonTypeEnum
+  ButtonTypeEnum,
 } from "../../../beans";
 
 /**
@@ -11,7 +11,7 @@ import {
 @Component({
   tag: "z-button",
   styleUrl: "styles.css",
-  shadow: true
+  shadow: true,
 })
 export class ZButton {
   @Element() hostElement: HTMLElement;
@@ -21,30 +21,36 @@ export class ZButton {
   /** name */
   @Prop() name?: string;
   /** disable button */
-  @Prop() disabled?: boolean = false;
+  @Prop({ reflect: true }) disabled?: boolean = false;
   /** button type */
   @Prop() type?: HTMLButtonElement["type"] = ButtonTypeEnum.button;
   /** graphic variant */
-  @Prop() variant?: ButtonVariantBean = ButtonVariantEnum.primary;
+  @Prop({ reflect: true }) variant?: ButtonVariantBean = ButtonVariantEnum.primary;
   /** reduce button size (optional) */
-  @Prop() issmall?: boolean = false;
+  @Prop({ reflect: true }) issmall?: boolean = false;
   /** add an icon to button (optional) */
   @Prop() icon?: string;
 
-  render() {
-    this.hostElement.style.pointerEvents = this.disabled ? "none" : "auto";
-
+  renderLegacyButton() {
     return (
       <button
         id={this.htmlid}
         name={this.name}
         type={this.type}
         disabled={this.disabled}
-        class={`${this.variant} ${this.issmall && "small"}`}
+        class={`${this.variant}${this.issmall ? " small" : ""}`}
       >
         {this.icon && <z-icon name={this.icon} width={16} height={16} />}
         <slot />
       </button>
+    );
+  }
+
+  render() {
+    return (
+      <slot name="element">
+        {this.renderLegacyButton()}
+      </slot>
     );
   }
 }
