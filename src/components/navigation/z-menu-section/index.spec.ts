6eab7b373e89e1a43a382c113e40a62f199d5ece
Dario <dariosacco9@gmail.com>
2021-04-28 16:03:50 +0200

    test: add tests

--- src/components/navigation/z-menu-section/index.spec.ts
+++ src/components/navigation/z-menu-section/index.spec.ts
@@ -3,23 +3,53 @@ import { newSpecPage } from "@stencil/core/testing";
 import { ZMenuSection } from "./index";
 
 describe("Suite test ZMenuSection", () => {
-  it("Test render ZMenuSection vuoto", async () => {
-
+  it("Test render ZMenu with items", async () => {
     const page = await newSpecPage({
       components: [ZMenuSection],
-      html: `<z-menu-section></z-menu-section>`
+      html: `<z-menu-section>
+        <h3>Menu section label</h3>
+        <a href="#" slot="item">Item 1</a>
+        <a href="#" slot="item">Item 2</a>
+        <a href="#" slot="item">Item 3</a>
+      </z-menu-section>`
     });
 
     expect(page.root).toEqualHtml(`
-      <z-menu-section>
+      <z-menu-section role="menu">
         <mock:shadow-root>
-          <div>
-            This is your z-menu-section component
-            This is your default prop:
-          </div>
+          <button aria-pressed="false" class="label">
+            <slot></slot>
+            <z-icon name="chevron-down"></z-icon>
+          </button>
         </mock:shadow-root>
-      </z-menu-section>
-    `)
+        <h3>Menu section label</h3>
+        <a href="#" slot="item">Item 1</a>
+        <a href="#" slot="item">Item 2</a>
+        <a href="#" slot="item">Item 3</a>
+    </z-menu-section>`)
+  });
+
+  it("Test ZMenuSection toggle events", async () => {
+    const page = await newSpecPage({
+      components: [ZMenuSection],
+      html: `<z-menu-section>
+        <div slot="item">item</div>
+      </z-menu-section>`
+    });
+    const menu = page.rootInstance;
+
+    const openedCallback = jest.fn();
+    page.doc.addEventListener('opened', openedCallback);
+    await page.waitForChanges();
+    // call `toggle` to open the menu
+    menu.toggle();
+    expect(openedCallback).toHaveBeenCalled();
+
+    const closedCallback = jest.fn();
+    page.doc.addEventListener('closed', closedCallback);
+    await page.waitForChanges();
+    // the menu is already open, this call will close it
+    menu.toggle();
+    expect(closedCallback).toHaveBeenCalled();
   });
 });
-
