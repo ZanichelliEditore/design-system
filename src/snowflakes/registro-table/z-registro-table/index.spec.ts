736be2ca22d18428d1cad5a9e0ff32cf4d29cbf7
abracale <a.bracale@spxlab.com>
2021-05-20 11:01:03 +0200

    fix: fix due PR

--- src/snowflakes/registro-table/z-registro-table/index.spec.ts
+++ src/snowflakes/registro-table/z-registro-table/index.spec.ts
@@ -6,18 +6,36 @@ describe("Suite test ZRegistroTable", () => {
   it("Test render ZRegistroTable con slot", async () => {
     const page = await newSpecPage({
       components: [ZRegistroTable],
-      html: `<z-registro-table><z-registro-table-head slot="tHeader"></z-registro-table-head></z-registro-table>`,
+      html: `<z-registro-table><z-registro-table-head></z-registro-table-head></z-registro-table>`,
     });
 
     expect(page.root).toEqualHtml(
       `<z-registro-table role="table">
-        <div>
-            <z-registro-table-head slot="tHeader"></z-registro-table-head>
+        <div class="table">
+            <z-registro-table-head></z-registro-table-head>
         </div>
       </z-registro-table>`
     );
   });
 
+  it("Test render ZRegistroTable con slot sticky-footer", async () => {
+    const page = await newSpecPage({
+      components: [ZRegistroTable],
+      html: `<z-registro-table><z-registro-table-head></z-registro-table-head><div slot="sticky-footer"></div></z-registro-table>`,
+    });
+
+    expect(page.root).toEqualHtml(
+      `<z-registro-table role="table">
+        <div class="table">
+            <z-registro-table-head></z-registro-table-head>
+        </div>
+        <div class="sticky-footer">
+            <div slot="sticky-footer"></div>
+        </div>
+      </z-registro-table>`
+    );
+  });
+  
   it("Test render ZRegistroTable con role table", async () => {
     const page = await newSpecPage({
       components: [ZRegistroTable],
--- src/snowflakes/registro-table/z-registro-table/index.tsx
+++ src/snowflakes/registro-table/z-registro-table/index.tsx
@@ -1,16 +1,18 @@
-import { Component, Element, Host, h } from "@stencil/core";
+import { Component, Element, Host, h, State } from "@stencil/core";
 
 @Component({
   tag: "z-registro-table",
   styleUrl: "styles.css",
   shadow: false,
-  scoped: true,
+  scoped: true
 })
 export class ZRegistroTable {
   @Element() host: HTMLElement;
+  @State() hasStickyFooter: boolean = false;
 
   componentWillRender() {
     this.host.setAttribute("role", "table");
+    this.hasStickyFooter = !!this.host.querySelector('[slot="sticky-footer"]');
   }
 
   render() {
@@ -19,9 +21,9 @@ export class ZRegistroTable {
             <div class="table">
                 <slot />
             </div>
-            <div class="sticky-footer">
-                <slot name="sticky-footer"/>
-            </div>
+            { this.hasStickyFooter && <div class="sticky-footer">
+                <slot name="sticky-footer" />
+            </div> }
         </Host>
     )
     
