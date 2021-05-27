cb232e70a728997c2e4c13781088d1e4c13ee99e
Andrea De Castri <adecastri@zanichelli.it>
2021-05-27 15:51:23 +0200

    test(clav-74): add test for z-popover

--- src/components/z-popover/index.spec.ts
+++ src/components/z-popover/index.spec.ts
@@ -3,23 +3,123 @@ import { newSpecPage } from "@stencil/core/testing";
 import { ZPopover } from "./index";
 
 describe("Suite test ZPopover", () => {
-  it("Test render ZPopover vuoto", async () => {
-
+  it("Test render ZPopover default", async () => {
     const page = await newSpecPage({
       components: [ZPopover],
-      html: `<z-popover></z-popover>`
+      html: `<z-popover><z-button slot="trigger">Trigger</z-button><div slot="popover">Content</div></z-popover>`,
     });
 
     expect(page.root).toEqualHtml(`
       <z-popover>
-        <mock:shadow-root>
           <div>
-            This is your z-popover component
-            This is your default prop:
+            <z-button slot="trigger">Trigger</z-button>
           </div>
-        </mock:shadow-root>
+          <div class="after-up border-radius-small popover-content-container shadow-1" style="background-color: var(--color-white);">
+            <div slot="popover">
+              Content
+            </div>
+          </div>
       </z-popover>
-    `)
+    `);
+  });
+
+  it("Test render ZPopover position prop", async () => {
+    const page = await newSpecPage({
+      components: [ZPopover],
+      html: `<z-popover position="after-center"><z-button slot="trigger">Trigger</z-button><div slot="popover">Content</div></z-popover>`,
+    });
+
+    expect(page.root).toEqualHtml(`
+      <z-popover position="after-center">
+          <div>
+            <z-button slot="trigger">Trigger</z-button>
+          </div>
+          <div class="after-center border-radius-small popover-content-container shadow-1" style="background-color: var(--color-white);">
+            <div slot="popover">
+              Content
+            </div>
+          </div>
+      </z-popover>
+    `);
+  });
+
+  it("Test render ZPopover radius prop", async () => {
+    const page = await newSpecPage({
+      components: [ZPopover],
+      html: `<z-popover border-radius="medium"><z-button slot="trigger">Trigger</z-button><div slot="popover">Content</div></z-popover>`,
+    });
+
+    expect(page.root).toEqualHtml(`
+      <z-popover border-radius="medium">
+          <div>
+            <z-button slot="trigger">Trigger</z-button>
+          </div>
+          <div class="after-up border-radius-medium popover-content-container shadow-1" style="background-color: var(--color-white);">
+            <div slot="popover">
+              Content
+            </div>
+          </div>
+      </z-popover>
+    `);
+  });
+
+  it("Test render ZPopover shadow prop", async () => {
+    const page = await newSpecPage({
+      components: [ZPopover],
+      html: `<z-popover box-shadow="shadow-4"><z-button slot="trigger">Trigger</z-button><div slot="popover">Content</div></z-popover>`,
+    });
+
+    expect(page.root).toEqualHtml(`
+      <z-popover box-shadow="shadow-4">
+          <div>
+            <z-button slot="trigger">Trigger</z-button>
+          </div>
+          <div class="after-up border-radius-small popover-content-container shadow-4" style="background-color: var(--color-white);">
+            <div slot="popover">
+              Content
+            </div>
+          </div>
+      </z-popover>
+    `);
+  });
+
+  it("Test render ZPopover background prop", async () => {
+    const page = await newSpecPage({
+      components: [ZPopover],
+      html: `<z-popover background-color="blue800"><z-button slot="trigger">Trigger</z-button><div slot="popover">Content</div></z-popover>`,
+    });
+
+    expect(page.root).toEqualHtml(`
+      <z-popover background-color="blue800">
+          <div>
+            <z-button slot="trigger">Trigger</z-button>
+          </div>
+          <div class="after-up border-radius-small popover-content-container shadow-1" style="background-color: var(--blue800);">
+            <div slot="popover">
+              Content
+            </div>
+          </div>
+      </z-popover>
+    `);
+  });
+
+  it("Test render ZPopover show-arrow prop", async () => {
+    const page = await newSpecPage({
+      components: [ZPopover],
+      html: `<z-popover show-arrow="true"><z-button slot="trigger">Trigger</z-button><div slot="popover">Content</div></z-popover>`,
+    });
+
+    expect(page.root).toEqualHtml(`
+      <z-popover show-arrow="true">
+          <div>
+            <z-button slot="trigger">Trigger</z-button>
+          </div>
+          <div class="after-up border-radius-small popover-content-container shadow-1 show-arrow" style="background-color: var(--color-white);">
+            <div slot="popover">
+              Content
+            </div>
+          </div>
+      </z-popover>
+    `);
   });
 });
-
