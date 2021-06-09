b0d888dc8e0ae43d53113ab6a40ffc532466a779
Andres Bautista <maxgonzalees@gmail.com>
2021-06-09 17:46:26 +0200

    feat: z-navigation tabs test

--- src/components/navigation/z-navigation-tabs/index.spec.ts
+++ src/components/navigation/z-navigation-tabs/index.spec.ts
@@ -4,4 +4,85 @@ import { ZNavigationTabs } from "./index";
 
 describe("Suite test ZNavigationTabs", () => {
 
+  it("Test render ZNavigationTab Horizontal", async () => {
+    const page = await newSpecPage({
+      components: [ZNavigationTabs],
+      html: `
+      <z-navigation-tabs orientation="horizontal" size="big">
+        <z-navigation-tab>
+          <z-icon name="house" slot="icon"></z-icon>
+          <span slot="label">Home</span>
+        </z-navigation-tab>
+      </z-navigation-tabs>
+    `});
+
+    expect(page.root).toEqualHtml(`
+      <z-navigation-tabs orientation="horizontal" size="big">
+        <mock:shadow-root>
+          <nav>
+            <slot></slot>
+          </nav>
+        </mock:shadow-root>
+        <z-navigation-tab orientation="horizontal">
+          <z-icon name="house" slot="icon"></z-icon>
+          <span slot="label">Home</span>
+        </z-navigation-tab>
+      </z-navigation-tabs>
+    `);
+  });
+
+  it("Test render ZNavigationTab Horizontal small", async () => {
+    const page = await newSpecPage({
+      components: [ZNavigationTabs],
+      html: `
+      <z-navigation-tabs orientation="horizontal" size="small">
+        <z-navigation-tab>
+          <z-icon name="house" slot="icon"></z-icon>
+          <span slot="label">Home</span>
+        </z-navigation-tab>
+      </z-navigation-tabs>
+    `});
+
+    expect(page.root).toEqualHtml(`
+      <z-navigation-tabs orientation="horizontal" size="small">
+        <mock:shadow-root>
+          <nav>
+            <slot></slot>
+          </nav>
+        </mock:shadow-root>
+        <z-navigation-tab orientation="horizontal">
+          <z-icon name="house" slot="icon"></z-icon>
+          <span slot="label">Home</span>
+        </z-navigation-tab>
+      </z-navigation-tabs>
+    `);
+  });
+
+  it("Test render ZNavigationTab Vertical", async () => {
+    const page = await newSpecPage({
+      components: [ZNavigationTabs],
+      html: `
+      <z-navigation-tabs orientation="vertical">
+        <z-navigation-tab>
+          <z-icon name="house" slot="icon"></z-icon>
+          <span slot="label">Home</span>
+        </z-navigation-tab>
+      </z-navigation-tabs>
+    `});
+
+    expect(page.root).toEqualHtml(`
+      <z-navigation-tabs orientation="vertical" size="big">
+        <mock:shadow-root>
+          <nav>
+            <slot></slot>
+          </nav>
+        </mock:shadow-root>
+        <z-navigation-tab orientation="vertical">
+          <z-icon name="house" slot="icon"></z-icon>
+          <span slot="label">Home</span>
+        </z-navigation-tab>
+      </z-navigation-tabs>
+    `);
+  });
+
 });
