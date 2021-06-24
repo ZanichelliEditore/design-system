a1a1173e6ec9495e8581ad71b42e3d1b9c1edf4d
Stefano Marcolini <stefano.marcolini@mondora.com>
2021-06-24 11:35:01 +0200

    test(CLAV-44): fix test

--- src/components/z-status-tag/index.spec.ts
+++ src/components/z-status-tag/index.spec.ts
@@ -6,11 +6,13 @@ describe("Suite test ZStatusTag", () => {
   it("Test render z-status-tag default", async () => {
     const page = await newSpecPage({
       components: [ZStatusTag],
-      html: `<z-status-tag></z-status-tag>`,
+      html: `<z-status-tag class="body-5-sb"></z-status-tag>`,
     });
 
     expect(page.root).toEqualHtml(
-      `<z-status-tag status="default"></z-status-tag>`
+      `<z-status-tag class="body-5-sb" status="default">
+        <mock:shadow-root></mock:shadow-root>
+      </z-status-tag>`
     );
   });
 
@@ -21,7 +23,11 @@ describe("Suite test ZStatusTag", () => {
     });
 
     expect(page.root).toEqualHtml(
-      `<z-status-tag status="default" icon="gear"><z-icon name="gear" /></z-status-tag>`
+      `<z-status-tag class="body-5-sb" status="default" icon="gear">
+        <mock:shadow-root>
+          <z-icon name="gear" />
+        </mock:shadow-root>
+      </z-status-tag>`
     );
   });
 
@@ -32,7 +38,11 @@ describe("Suite test ZStatusTag", () => {
     });
 
     expect(page.root).toEqualHtml(
-      `<z-status-tag status="default" text="in progress"><span class="body-5-sb">in progress</span></z-status-tag>`
+      `<z-status-tag class="body-5-sb" status="default" text="in progress">
+        <mock:shadow-root>
+          <span>in progress</span>
+        </mock:shadow-root>
+      </z-status-tag>`
     );
   });
 
@@ -43,9 +53,11 @@ describe("Suite test ZStatusTag", () => {
     });
 
     expect(page.root).toEqualHtml(
-      `<z-status-tag icon="gear" status="default" text="in progress">
-        <z-icon name="gear"></z-icon>
-        <span class="body-5-sb">in progress</span>
+      `<z-status-tag class="body-5-sb" icon="gear" status="default" text="in progress">
+        <mock:shadow-root>
+          <z-icon name="gear"></z-icon>
+          <span>in progress</span>
+        </mock:shadow-root>
       </z-status-tag>`
     );
   });
@@ -57,7 +69,9 @@ describe("Suite test ZStatusTag", () => {
     });
 
     expect(page.root).toEqualHtml(
-      `<z-status-tag status="default" expandable="true"></z-status-tag>`
+      `<z-status-tag class="body-5-sb" status="default" expandable="true">
+        <mock:shadow-root></mock:shadow-root>
+      </z-status-tag>`
     );
   });
 
@@ -68,7 +82,9 @@ describe("Suite test ZStatusTag", () => {
     });
 
     expect(page.root).toEqualHtml(
-      `<z-status-tag status="warning"></z-status-tag>`
+      `<z-status-tag class="body-5-sb" status="warning">
+        <mock:shadow-root></mock:shadow-root>
+      </z-status-tag>`
     );
   });
 
@@ -79,7 +95,9 @@ describe("Suite test ZStatusTag", () => {
     });
 
     expect(page.root).toEqualHtml(
-      `<z-status-tag class="dark" status="default" variant="dark"></z-status-tag>`
+      `<z-status-tag class="body-5-sb dark" status="default" variant="dark">
+        <mock:shadow-root></mock:shadow-root>
+      </z-status-tag>`
     );
   });
 });
