94fbc459306cc4efe9330178704653c906c1823e
andreadecastri <adecastri@zanichelli.it>
2021-06-24 15:09:29 +0200

    test(clav-41): edit default text for extended row

--- src/snowflakes/registro-table/z-registro-table-expanded-row/index.spec.ts
+++ src/snowflakes/registro-table/z-registro-table-expanded-row/index.spec.ts
@@ -4,22 +4,22 @@ import { ZRegistroTableExpandedRow } from "./index";
 
 describe("Suite test ZRegistroTableExpandedRow", () => {
   it("Test render ZRegistroTableExpandedRow vuoto", async () => {
-
     const page = await newSpecPage({
       components: [ZRegistroTableExpandedRow],
-      html: `<z-registro-table-expanded-row></z-registro-table-expanded-row>`
+      html: `<z-registro-table-expanded-row col-span="4"></z-registro-table-expanded-row>`,
     });
 
     expect(page.root).toEqualHtml(`
-      <z-registro-table-expanded-row>
+      <z-registro-table-expanded-row col-span="4">
         <mock:shadow-root>
-          <div>
-            This is your z-registro-table-expanded-row component
-            This is your default prop:
-          </div>
+          <td colspan="4">
+            <div class="content-container">
+              <div class="inner-content">
+                <slot></slot>
+              </div>
+            </div>
+          </td>
         </mock:shadow-root>
-      </z-registro-table-expanded-row>
-    `)
+      </z-registro-table-expanded-row>`);
   });
 });
-
