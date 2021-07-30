f4cd85a51270db6a4fb2ef5588a84ec545bc4dd2
saldomik <saldomik@gmail.com>
2021-07-30 10:49:04 +0200

    Component - added event handling

--- src/components/z-toast-notification/index.tsx
+++ src/components/z-toast-notification/index.tsx
@@ -2,9 +2,12 @@ import {
   Component,
   Prop,
   h,
+  Event,
+  EventEmitter,
   Element,
 } from "@stencil/core";
 import { ToastNotificationEnum, ToastNotificationTypes } from "../../beans";
+import { mobileBreakpoint } from "../../constants/breakpoints";
 
 @Component({
   tag: "z-toast-notification",
@@ -20,16 +23,65 @@ export class ZToastNotification {
   @Prop() autoclose?: number;
   @Prop() type?: ToastNotificationTypes;
 
+  private toastText: HTMLElement;
+  private toastContainer: HTMLElement;
+
+  /** notification close event */
+  @Event() toastClose: EventEmitter;
+  emitToastClose() {
+    this.toastClose.emit();
+    console.log("toast closed!");
+  }
+
+  @Event() toastAction: EventEmitter;
+  emitToastAction() {
+    this.toastAction.emit();
+    console.log("toast action!");
+  }
+
+  componentDidLoad() {
+    this.setCorrectPadding();
+
+    if (this.autoclose && typeof this.autoclose === "number") {
+      this.startClosingTimeout(this.autoclose);
+    }
+
+    if (window.innerWidth <= mobileBreakpoint) {
+      this.setMobileView();
+    }
+  }
+
+  setCorrectPadding() {
+    if (this.toastText.offsetHeight > 20) {
+      this.toastContainer.style.padding = "16px";
+    } else {
+      this.toastContainer.style.padding = "8px 16px";
+    }
+  }
+
+  setMobileView() {
+    if (this.toastText.offsetHeight > 20) {
+      this.toastContainer.style.gridTemplateColumns = "4fr 0fr";
+      this.toastContainer.style.gridTemplateRows = "auto 1fr";
+      this.toastContainer.style.gridTemplateAreas = '"testo testo testo testo icona" "tasto"';
+    }
+  }
+
+  startClosingTimeout(time: number) {
+    setTimeout(() => this.emitToastClose(), time);
+  }
+
   render() {
     return (
       <div
         class={this.type ? this.type : ToastNotificationEnum.dark}
+        ref={(el) => (this.toastContainer = el as HTMLElement)}
       >
-        <div id="text">
+        <div id="text" ref={(el) => (this.toastText = el as HTMLElement)}>
           <span class="title">{this.titolo}</span>
           <span class="message">{this.message}</span>
         </div>
-        <div id="button">
+        <div id="button" onClick={() => this.emitToastAction()}>
           <slot name="button" />
         </div>
         <div id="icon">
@@ -38,7 +90,7 @@ export class ZToastNotification {
               name="multiply-circled"
               width={15}
               height={15}
-              
+              onClick={() => this.emitToastClose()}
             />
           )}
         </div>
