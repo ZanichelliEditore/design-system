807171444731aabd7e255ff0c20defe48bb66756
greta.lucchina <greta.lucchina@mondora.com>
2021-08-25 19:24:14 +0200

    test(clav-43): fix test

--- src/components/contextual-menu/index.spec.ts
+++ src/components/contextual-menu/index.spec.ts
@@ -10,7 +10,7 @@ describe("Suite test ContextualMenu", () => {
     });
 
     expect(page.root).toEqualHtml(`
-      <contextual-menu>
+      <contextual-menu tabindex="0">
         <mock:shadow-root>
         <z-popover background-color="gray50" box-shadow="shadow-2" padding="0" position="after-down">
                <z-icon fill="var(--color-primary01)" name="contextual-menu" slot="trigger" style="cursor: pointer;"></z-icon>
@@ -31,14 +31,14 @@ describe("Suite test ContextualMenu", () => {
       html: `<contextual-menu elements='[{"icon":"share","text":"Element 1","key": "0"},{"icon":"delete","text":"Elemento 2","key": "1"}]''></contextual-menu>`,
     });
     expect(page.root).toEqualHtml(`
-      <contextual-menu '="" elements='[{"icon":"share","text":"Element 1","key": "0"},{"icon":"delete","text":"Elemento 2","key": "1"}]'>
+      <contextual-menu '="" elements='[{"icon":"share","text":"Element 1","key": "0"},{"icon":"delete","text":"Elemento 2","key": "1"}]' tabindex="0">
         <mock:shadow-root>
          <z-popover background-color="gray50" box-shadow="shadow-2" padding="0" position="after-down">
                  <z-icon fill="var(--color-primary01)" name="contextual-menu" slot="trigger" style="cursor: pointer;"></z-icon>
                  <div class="popover-content-container" slot="popover">
                    <z-list>
                      <z-list-group divider-type="element" size="small">
-                       <z-list-element align-button="left" class="my-z-list-element" clickable="" color="var(--color-primary01)" expandable-style="accordion" iscontextualmenu="">
+                       <z-list-element align-button="left" class="my-z-list-element" clickable="" color="var(--color-primary01)" expandable-style="accordion" iscontextualmenu="" listelementid="0">
                          <div class="element-container">
                            <div class="icon-container">
                              <z-icon fill="var(--color-primary01)" name="share"></z-icon>
@@ -50,7 +50,7 @@ describe("Suite test ContextualMenu", () => {
                            </div>
                          </div>
                        </z-list-element>
-                       <z-list-element align-button="left" class="my-z-list-element" clickable="" color="var(--color-primary01)" expandable-style="accordion" iscontextualmenu="">
+                       <z-list-element align-button="left" class="my-z-list-element" clickable="" color="var(--color-primary01)" expandable-style="accordion" iscontextualmenu="" listelementid="1">
                          <div class="element-container">
                            <div class="icon-container">
                              <z-icon fill="var(--color-primary01)" name="delete"></z-icon>
