9c983d3496c630bffd40895112ac3914acfe0800
Paolo F. Marino <p.marino@esis-italia.com>
2021-08-31 15:28:09 +0200

    feat: add notifation-list tests

--- src/components/notification/z-toast-notification-list/index.spec.ts
+++ src/components/notification/z-toast-notification-list/index.spec.ts
@@ -3,7 +3,7 @@ import { newSpecPage } from "@stencil/core/testing";
 import { ZToastNotificationList } from "./index";
 
 describe("Suite test ZToastNotificationList", () => {
-  it("Test render ZToastNotificationList vuoto", async () => {
+  it("Test render ZToastNotificationList vuoto senza props", async () => {
 
     const page = await newSpecPage({
       components: [ZToastNotificationList],
@@ -13,8 +13,60 @@ describe("Suite test ZToastNotificationList", () => {
     expect(page.root).toEqualHtml(`
       <z-toast-notification-list class="top-right">
         <mock:shadow-root>
-          <slot></slot>
+          <div class="newest-on-top" id="notification-stack">
+            <slot></slot>
+          </div>
         </mock:shadow-root>
+      </z-toast-notification-list>
+    `)
+  });
+
+
+  it("Test render ZToastNotificationList vuoto con props", async () => {
+
+    const page = await newSpecPage({
+      components: [ZToastNotificationList],
+      html: `<z-toast-notification-list newestontop="false" position="top-centre"></z-toast-notification-list>`
+    });
+
+    expect(page.root).toEqualHtml(`
+      <z-toast-notification-list newestontop="false" position="top-centre" class="top-centre">
+        <mock:shadow-root>
+          <div id="notification-stack">
+            <slot></slot>
+          </div>
+        </mock:shadow-root>
+      </z-toast-notification-list>
+    `)
+  });
+
+  it("Test render ZToastNotificationList con elementi slottati e props", async () => {
+
+    const page = await newSpecPage({
+      components: [ZToastNotificationList],
+      html: `<z-toast-notification-list position="top-centre">
+        <z-toast-notification type="error" heading="Notification" message="Senza pulsante."
+            transition="slide-in-up" draggablepercentage="50" closebutton>
+        </z-toast-notification>
+        <z-toast-notification type="error" heading="Notification" message="Senza pulsante."
+            transition="slide-in-up" draggablepercentage="50" closebutton>
+        </z-toast-notification>
+      </z-toast-notification-list>`
+    });
+
+    expect(page.root).toEqualHtml(`
+      <z-toast-notification-list position="top-centre" class="top-centre">
+        <mock:shadow-root>
+          <div class="newest-on-top" id="notification-stack">
+            <slot></slot>
+          </div>
+        </mock:shadow-root>
+        <z-toast-notification type="error" heading="Notification" message="Senza pulsante."
+          transition="slide-in-up" draggablepercentage="50" closebutton>
+        </z-toast-notification>
+        <z-toast-notification type="error" heading="Notification" message="Senza pulsante."
+          transition="slide-in-up" draggablepercentage="50" closebutton>
+        </z-toast-notification>
       </z-toast-notification-list>
     `)
   });
