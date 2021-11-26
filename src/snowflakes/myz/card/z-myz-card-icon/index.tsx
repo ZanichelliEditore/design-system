a99a269e1a35a4a36a20613fe6909cd3b657a349
Axl Fernandez <a.fernandez@esis-italia.com>
2021-11-26 16:34:24 +0100

    fix:z-myz-card-icon button missing aria label

--- src/snowflakes/myz/card/z-myz-card-icon/index.tsx
+++ src/snowflakes/myz/card/z-myz-card-icon/index.tsx
@@ -10,10 +10,12 @@ export class ZMyzCardIcon {
   @Prop() icon: string;
   /** disabled status flag */
   @Prop() isdisabled: boolean = false;
+  /** description of the icon*/
+  @Prop() ariaLabel?: string;
 
   render() {
     return (
-      <button disabled={this.isdisabled}>
+      <button disabled={this.isdisabled} aria-label={this.ariaLabel}>
         <span>
           <z-icon name={this.icon} width={16} height={16} />
         </span>
