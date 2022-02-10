d6289fb3668ba0ad9d2ca25ce00533d2a4f238ea
Axl Fernandez <a.fernandez@esis-italia.com>
2022-02-10 14:31:06 +0100

    MYZ-8255: filetype prop and icon handling

--- src/components/file-upload/z-file/index.tsx
+++ src/components/file-upload/z-file/index.tsx
@@ -1,30 +1,61 @@
-import { Component, h, EventEmitter, Event, Element } from '@stencil/core';
+import {
+  Component,
+  h,
+  EventEmitter,
+  Event,
+  Element,
+  Prop,
+} from "@stencil/core";
 
 @Component({
-  tag: 'z-file',
-  styleUrl: 'styles.css',
-  shadow: true
+  tag: "z-file",
+  styleUrl: "styles.css",
+  shadow: true,
 })
-
 export class ZFile {
   @Element() el: HTMLElement;
 
+  /** Prop which indicates the type of the file */
+  @Prop() filetype: string = "";
+
   /** Emitted when a z-file component is removed from the DOM */
-  @Event() removeFile: EventEmitter
+  @Event() removeFile: EventEmitter;
   removeFileHandler() {
     this.removeFile.emit();
     this.el.remove();
   }
 
+  /**  */
+  handleChipIcon() {
+    switch (this.filetype) {
+      case "application/pdf":
+        return "pdf";
+      case "image/tiff":
+        return "image-tiff";
+      case "image/png":
+        return "image-png";
+      case "image/jpeg":
+      case "image/jpg":
+        return "image-jpg";
+      default:
+        return "document-with-text";
+    }
+  }
+
   render() {
     return (
-        <z-chip>
-          <div class="chip-content">
-            <z-icon name="document-with-text" />
-            <slot />
-            <z-icon onClick={() => this.removeFileHandler()} name="multiply-circled" height={15} width={15} />
-          </div>
-        </z-chip>
+      <z-chip>
+        <div class="chip-content">
+          <z-icon name={this.handleChipIcon()} />
+          <slot />
+          <z-icon
+            onClick={() => this.removeFileHandler()}
+            name="multiply-circled"
+            height={15}
+            width={15}
+          />
+        </div>
+      </z-chip>
     );
   }
 }
--- src/components/file-upload/z-file/readme.md
+++ src/components/file-upload/z-file/readme.md
@@ -5,6 +5,13 @@
 <!-- Auto Generated Below -->
 
 
+## Properties
+
+| Property   | Attribute  | Description                               | Type     | Default |
+| ---------- | ---------- | ----------------------------------------- | -------- | ------- |
+| `filetype` | `filetype` | Prop which indicates the type of the file | `string` | `""`    |
+
+
 ## Events
 
 | Event        | Description                                             | Type               |
