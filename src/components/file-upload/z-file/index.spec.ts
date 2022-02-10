cebd829cafb82429d86262377084baed561d61ff
Axl Fernandez <a.fernandez@esis-italia.com>
2022-02-10 14:33:49 +0100

    MYZ-8255: z-file test

--- src/components/file-upload/z-file/index.spec.ts
+++ src/components/file-upload/z-file/index.spec.ts
@@ -13,13 +13,54 @@ describe("Suite test ZFile", () => {
     expect(page.root).toEqualHtml(`
       <z-file>
         <mock:shadow-root>
-          <div>
-            This is your z-file component
-            This is your default prop:
+        <z-chip>
+          <div class="chip-content">
+            <z-icon name="document-with-text"></z-icon>
+            <slot></slot>
+            <z-icon height="15" name="multiply-circled" width="15"></z-icon>
           </div>
+        </z-chip>
         </mock:shadow-root>
       </z-file>
     `)
   });
+
+  it("Test render ZFile with filetype prop", async () => {
+
+    const page = await newSpecPage({
+      components: [ZFile],
+      html: `<z-file filetype="image/jpeg"></z-file>`
+    });
+
+    expect(page.root).toEqualHtml(`
+      <z-file filetype="image/jpeg">
+        <mock:shadow-root>
+        <z-chip>
+          <div class="chip-content">
+            <z-icon name="image-jpg"></z-icon>
+            <slot></slot>
+            <z-icon height="15" name="multiply-circled" width="15"></z-icon>
+          </div>
+        </z-chip>
+        </mock:shadow-root>
+      </z-file>
+    `)
+  });
+
+  /**
+   * test 5 types of possibile zfile
+   *
+      case "application/pdf":
+        return "pdf";
+      case "application/doc":
+        return "document-with-text";
+      case "image/tiff":
+        return "image-tiff";
+      case "image/png":
+        return "image-png";
+      case "image/jpeg":
+      case "image/jpg":
+        return "image-jpg";
+   */
 });
 
