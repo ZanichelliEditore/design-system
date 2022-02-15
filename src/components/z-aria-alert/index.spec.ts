0b5963d5cd444e87a2ff72c26755a10f78addcca
Francesco Chiapella <francesco.chiapella@mondora.com>
2022-02-15 09:41:43 +0100

    test(ZTE-4456): add aria alert test

--- src/components/z-aria-alert/index.spec.ts
+++ src/components/z-aria-alert/index.spec.ts
@@ -4,16 +4,15 @@ import { ZAriaAlert } from "./index";
 
 describe("Suite test ZAriaAlert", () => {
   it("Test render ZAriaAlert vuoto", async () => {
-
     const page = await newSpecPage({
       components: [ZAriaAlert],
-      html: `<z-aria-alert></z-aria-alert>`
+      html: `<z-aria-alert>testo</z-aria-alert>`,
     });
 
-    expect(page.root).toEqualHtml(`
-      <z-aria-alert>
-      </z-aria-alert>
-    `)
+    expect(page.root).toEqualHtml(
+      `<z-aria-alert aria-atomic="true" aria-live="polite" aria-relevant="additions">
+         testo
+       </z-aria-alert>`
+    );
   });
 });
-
