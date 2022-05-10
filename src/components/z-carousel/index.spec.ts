64d9aa3bfb54c23d97ecd8cfe5820566fc35a67d
Stefano Marcolini <stefanomarcolini@MacBook-Pro-di-Stefano-2.local>
2022-05-10 15:11:52 +0200

    test(CLAV-275/Zcarousel): fix test

--- src/components/z-carousel/index.spec.ts
+++ src/components/z-carousel/index.spec.ts
@@ -3,27 +3,34 @@ import { newSpecPage } from "@stencil/core/testing";
 import { ZCarousel } from "./index";
 
 describe("Suite test ZCarousel", () => {
-  it("Test render ZCarousel vuoto", async () => {
+  it("Test render ZCarousel", async () => {
     const page = await newSpecPage({
       components: [ZCarousel],
       html: `<z-carousel></z-carousel>`,
     });
 
     expect(page.root).toEqualHtml(`
-      <z-carousel>
-      <ul class="carousel-item-container"></ul>
-      </z-carousel>
+    <z-carousel>
+    <div>
+      <ul class="z-carousel-items-container"></ul>
+    </div>
+  </z-carousel>
     `);
   });
 
   it("Test render ZCarousel elements prop", async () => {
     const page = await newSpecPage({
       components: [ZCarousel],
-      html: `<z-carousel gutter=30></z-carousel>`,
+      html: `<z-carousel><li><div /></li></z-carousel>`,
     });
-    expect(page.root).toEqualHtml(`
-    <z-carousel gutter=30>
-    <ul class="carousel-item-container"></ul>
+    expect(page.root).toEqualHtml(`<z-carousel>
+    <div>
+      <ul class="z-carousel-items-container">
+        <li>
+          <div></div>
+        </li>
+      </ul>
+    </div>
   </z-carousel>
     `);
   });
