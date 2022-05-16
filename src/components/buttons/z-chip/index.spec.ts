6bf5c687d07a60c71a85fc5a30708fb67042eb6c
Paolo F. Marino <p.marino@esis-italia.com>
2022-05-16 11:31:41 +0200

    chore: add tests

--- src/components/buttons/z-chip/index.spec.ts
+++ src/components/buttons/z-chip/index.spec.ts
@@ -6,127 +6,161 @@ describe("Suite test ZChip", () => {
   it("Test render ZChip empty", async () => {
     const page = await newSpecPage({
       components: [ZChip],
-      html: `<z-chip type="default"></z-chip>`,
+      html: `<z-chip icon-position="left" type="default"></z-chip>`,
     });
 
     expect(page.root).toEqualHtml(`
-        <z-chip type="default">
-          <mock:shadow-root>
-          <div class="default">
-          <slot></slot>&nbsp;
+      <z-chip icon-position="left" type="default">
+        <mock:shadow-root>
+          <div class="default left">
+            <span class="bold label"></span>
+            <span>
+              <slot></slot>
+            </span>
           </div>
         </mock:shadow-root>
       </z-chip>
     `);
   });
 
-  it("Test render ZChip with attributes", async () => {
+  it("Test render ZChip with label", async () => {
     const page = await newSpecPage({
       components: [ZChip],
-      html: `<z-chip boldtext="20" regulartext="libri trovati" type="default"></z-chip>`,
+      html: `<z-chip label="label" type="default">text</z-chip>`,
     });
 
     expect(page.root).toEqualHtml(`
-        <z-chip type="default" boldtext="20" regulartext="libri trovati">
+        <z-chip label="label" icon-position="left" type="default">
           <mock:shadow-root>
-            <div>
-              <span class="boldtext">20</span>&nbsp;libri trovati
+            <div class="default left">
+              <span class="bold label">label</span>
+              <span>
+                <slot></slot>
+              </span>
             </div>
           </mock:shadow-root>
+          text
         </z-chip>
     `);
   });
 
-  it("Test render ZChip with children", async () => {
+  it("Test render ZChip with text bold", async () => {
     const page = await newSpecPage({
       components: [ZChip],
-      html: `<z-chip type="default"><z-body>ciao 1</z-body></z-chip>`,
+      html: `<z-chip bold label="label" icon-position="left" type="default">text</z-chip>`,
     });
 
     expect(page.root).toEqualHtml(`
-        <z-chip type="default">
+        <z-chip bold="" label="label" icon-position="left" type="default">
           <mock:shadow-root>
-            <div class="default">
-              <slot />
+            <div class="default left">
+              <span class="bold label">label</span>
+              <span class="bold">
+                <slot></slot>
+              </span>
             </div>
           </mock:shadow-root>
-          <z-body>ciao 1</z-body>
+          text
         </z-chip>
     `);
   });
 
-  it("Test render ZChip with attributes and children", async () => {
+  it("Test render ZChip icon-position right type medium", async () => {
     const page = await newSpecPage({
       components: [ZChip],
-      html: `<z-chip type="default" boldtext="20" regulartext="libri trovati">
-               <z-body>ciao 1</z-body>
-             </z-chip>`,
+      html: `<z-chip bold label="label" icon-position="right" type="medium">text</z-chip>`,
     });
 
     expect(page.root).toEqualHtml(`
-        <z-chip type="default" boldtext="20" regulartext="libri trovati">
+        <z-chip bold="" label="label" icon-position="right" type="medium">
           <mock:shadow-root>
-            <div>
-              <span class="boldtext">20</span>&nbsp;libri trovati
+            <div class="medium right">
+              <span class="bold label">label</span>
+              <span class="bold">
+                <slot></slot>
+              </span>
             </div>
           </mock:shadow-root>
-          <z-body>ciao 1</z-body>
+          text
         </z-chip>
     `);
   });
 
-  it("Test render ZChip with attributes and children and type mini", async () => {
+  it("Test render ZChip icon-position right type medium no bold", async () => {
     const page = await newSpecPage({
       components: [ZChip],
-      html: `<z-chip type="mini" boldtext="20" regulartext="libri trovati">
-               <z-body>ciao 1</z-body>
-             </z-chip>`,
+      html: `<z-chip label="label" icon-position="right" type="medium">text</z-chip>`,
     });
 
     expect(page.root).toEqualHtml(`
-        <z-chip type="mini" boldtext="20" regulartext="libri trovati">
+        <z-chip label="label" icon-position="right" type="medium">
           <mock:shadow-root>
-            <div>
-              <span class="boldtext">20</span>&nbsp;libri trovati
+            <div class="medium right">
+              <span class="bold label">label</span>
+              <span>
+                <slot></slot>
+              </span>
             </div>
           </mock:shadow-root>
-          <z-body>ciao 1</z-body>
+          text
         </z-chip>
     `);
   });
 
-  it("Test render ZChip FilterChip with children", async () => {
+  it("Test render ZChip interaction", async () => {
     const page = await newSpecPage({
       components: [ZChip],
-      html: `<z-chip type="default" filter ><z-body>ciao 1</z-body></z-chip>`,
+      html: `<z-chip interaction icon-position="left" type="default">text</z-chip>`,
     });
 
     expect(page.root).toEqualHtml(`
-        <z-chip type="default" filter>
+        <z-chip icon-position="left" interaction="" type="default">
           <mock:shadow-root>
             <button class="default" tabindex="0">
-              <slot />
+                <slot></slot>
+              <z-icon height="14" name="multiply-circled" tabindex="0" width="14"></z-icon>
             </button>
           </mock:shadow-root>
-          <z-body>ciao 1</z-body>
+          text
         </z-chip>
     `);
   });
 
-  it("Test render ZChip FilterChip with children disabled", async () => {
+  it("Test render ZChip interaction disabled", async () => {
     const page = await newSpecPage({
       components: [ZChip],
-      html: `<z-chip type="default" filter disabled><z-body>ciao 1</z-body></z-chip>`,
+      html: `<z-chip disabled interaction icon-position="left" type="default">text</z-chip>`,
     });
 
     expect(page.root).toEqualHtml(`
-        <z-chip type="default" filter disabled>
+        <z-chip disabled="" icon-position="left" interaction="" type="default">
           <mock:shadow-root>
             <button class="default" disabled="" tabindex="0">
-              <slot />
+                <slot></slot>
+              <z-icon height="14" name="multiply-circled" tabindex="0" width="14"></z-icon>
             </button>
           </mock:shadow-root>
-          <z-body>ciao 1</z-body>
+          text
+        </z-chip>
+    `);
+  });
+
+  it("Test render ZChip interaction with icon", async () => {
+    const page = await newSpecPage({
+      components: [ZChip],
+      html: `<z-chip icon="pdf" interaction icon-position="left" type="default">text</z-chip>`,
+    });
+
+    expect(page.root).toEqualHtml(`
+        <z-chip icon="pdf" icon-position="left" interaction="" type="default">
+          <mock:shadow-root>
+            <button class="default"  tabindex="0">
+              <z-icon height="14" name="pdf" width="14"></z-icon>
+                <slot></slot>
+              <z-icon height="14" name="multiply-circled" tabindex="0" width="14"></z-icon>
+            </button>
+          </mock:shadow-root>
+          text
         </z-chip>
     `);
   });
