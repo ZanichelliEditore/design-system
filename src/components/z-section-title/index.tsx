58551f1bb7fc8cc79b20f4b0537cc846a73db946
Dario <dariosacco9@gmail.com>
2022-05-19 15:01:15 +0200

    chore: more docs

--- src/components/z-section-title/index.tsx
+++ src/components/z-section-title/index.tsx
@@ -12,7 +12,7 @@ import {
  * @slot secondary-title - Slot for the secondary title. When present, the divider is always displayed under it,
  * following its width and removing the one for the primary title.
  * @slot primary-title - Slot for the primary title.
- * @cssprop --z-section-title--divider-color - Color of the divider.
+ * @cssprop --z-section-title--divider-color - Color of the divider. Use a CSS prop string like `"--red500"`. Default is `"--red500"`.
  */
 @Component({
   tag: "z-section-title",
