64e8e08a197d4a759077078641c00c7d4897431e
Dario <dariosacco9@gmail.com>
2022-05-20 18:20:53 +0200

    refactor: alignments; stories; implementation

--- src/components/notification/z-notification/index.tsx
+++ src/components/notification/z-notification/index.tsx
@@ -1,86 +1,101 @@
 import { Component, Prop, h, Event, EventEmitter } from '@stencil/core';
-
 import { NotificationType } from '../../../beans';
 
 /**
- * @slot - the content of the notification
+ * Notification bar component.
+ * @slot - the text of the notification
  */
 @Component({
   tag: 'z-notification',
   styleUrl: 'styles.css',
-  shadow: true
+  shadow: false,
+  scoped: true,
 })
-
 export class ZNotification {
-  /** icon on the left of the content  */
-  @Prop() contenticonname?: string;
-  /** action button text */
-  @Prop() actiontext?: string;
-  /** alert variant type */
-  @Prop() type: NotificationType;
-  /** enable close icon */
-  @Prop() showclose?: boolean = false;
-  /** enable shadow */
-  @Prop() showshadow?: boolean = false;
+  /** Name of the icon on the left of the content */
+  @Prop()
+  contenticonname?: string;
 
+  /** Action button text */
+  @Prop()
+  actiontext?: string;
 
-  /** notification action event */
+  /** Alert variant type */
+  @Prop({ reflect: true })
+  type: NotificationType;
+
+  /** Enable close icon */
+  @Prop()
+  showclose?: boolean = false;
+
+  /**
+   * Enable shadow.
+   * @deprecated shadow is available only for the `sticky` version of the notification.
+   */
+  @Prop({ reflect: true })
+  showshadow?: boolean = false;
+
+  /** Enable sticky notification bar. */
+  @Prop({ reflect: true })
+  sticky?: boolean = false;
+
+  /** Call to action clicked */
   @Event() notificationAction: EventEmitter;
+
   handleActionButtonClick(e: MouseEvent): void {
     e.preventDefault();
     this.notificationAction.emit();
   }
 
-  /** notification close event */
+  /** Close button clicked */
   @Event() notificationClose: EventEmitter;
+
   handleCloseButtonClick(e: MouseEvent): void {
     e.preventDefault();
     this.notificationClose.emit();
   }
 
+  constructor() {
+    this.handleActionButtonClick = this.handleActionButtonClick.bind(this);
+    this.handleCloseButtonClick = this.handleCloseButtonClick.bind(this);
+  }
+
   render() {
-    return (
-      <div class={{
-        "notification-container": true,
-        "success-notification": this.type === NotificationType.success,
-        "warning-notification": this.type === NotificationType.warning,
-        "error-notification": this.type === NotificationType.error,
-        "shadow": this.showshadow
-      }}>
-        {this.contenticonname && (
-          <z-icon
-            name={this.contenticonname}
-            width={16}
-            height={16}
-          />
-        )}
-        <div class="content-container">
-          <z-body class="content-text" level={4}><slot></slot></z-body>
-          {this.actiontext && !!this.actiontext.trim().length && (
-            <z-body
-              class="action-text"
-              role="button"
-              tabindex="0"
-              onClick={(e: MouseEvent) => {
-                this.handleActionButtonClick(e);
-              }}
-              level={5}
-              variant="semibold"
-            >
-              {this.actiontext}
-            </z-body>
-          )}
+    return [
+      this.contenticonname && <z-icon
+        class="status-icon"
+        name={this.contenticonname}
+        width={16}
+        height={16}
+      />,
+
+      <div class="content-container">
+        <div class="content-text body-4">
+          <slot></slot>
         </div>
-        {this.showclose && (
-          <z-icon
-            class="close-icon"
-            name="multiply-circle"
-            width={16}
-            height={16}
-            onClick={(e: MouseEvent) => this.handleCloseButtonClick(e)}
-          />
-        )}
-      </div>
-    );
+
+        {!!this.actiontext?.trim() &&
+          <button
+            class="action-button interactive-3-sb"
+            type="button"
+            onClick={this.handleActionButtonClick}
+          >
+            {this.actiontext}
+          </button>
+        }
+      </div>,
+
+      this.showclose && <button
+        class="close-button"
+        type="button"
+        onClick={this.handleCloseButtonClick}
+      >
+        <z-icon
+          name="multiply-circle"
+          width={16}
+          height={16}
+        />
+      </button>
+    ];
   }
 }
