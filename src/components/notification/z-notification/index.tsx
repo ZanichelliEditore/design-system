3b86ff4e5ed6ac2a3017ec05f954d84d653dd0cb
Dario <dariosacco9@gmail.com>
2022-05-23 14:33:30 +0200

    feat: add top-offset css prop; fix stories and tests

--- src/components/notification/z-notification/index.tsx
+++ src/components/notification/z-notification/index.tsx
@@ -3,7 +3,8 @@ import { NotificationType } from '../../../beans';
 
 /**
  * Notification bar component.
- * @slot - the text of the notification
+ * @slot - The text of the notification.
+ * @cssprop --z-notification--top-offset - The top offset of the notification. Use it when `sticky` prop is set to `true` and you need the notification to stay under other sticky elements. Default: 0px.
  */
 @Component({
   tag: 'z-notification',
