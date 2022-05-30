bb0d76d17e1afb9bafd6543580ced5f24440019c
PFM <p.marino@esis-italia.com>
2022-05-30 09:34:53 +0200

    refactor: fix tests

--- src/components/z-tag/index.spec.ts
+++ src/components/z-tag/index.spec.ts
@@ -9,7 +9,9 @@ describe("Suite test ZTag", () => {
       html: `<z-tag class="body-5-sb"></z-tag>`,
     });
 
-    expect(page.root).toEqualHtml(`<z-tag class="body-5-sb"></z-tag>`);
+    expect(page.root).toEqualHtml(
+      `<z-tag class="body-5-sb"><div></div></z-tag>`
+    );
   });
 
   it("Test render z-tag with icon", async () => {
@@ -20,21 +22,20 @@ describe("Suite test ZTag", () => {
 
     expect(page.root).toEqualHtml(`
       <z-tag class="body-5-sb" icon="gear">
-        <z-icon name="gear"></z-icon>
+        <z-icon name="gear" height="15" width="15"></z-icon>
+        <div></div>
       </z-tag>`);
   });
 
   it("Test render z-tag with text", async () => {
     const page = await newSpecPage({
       components: [ZTag],
-      html: `<z-tag class="body-5-sb" text="no progress"></z-tag>`,
+      html: `<z-tag class="body-5-sb">my custom element</z-tag>`,
     });
 
     expect(page.root).toEqualHtml(`
-      <z-tag class="body-5-sb" text="no progress">
-        <span>
-          no progress
-        </span>
+      <z-tag class="body-5-sb">
+      <div>my custom element</div>
       </z-tag>
     `);
   });
@@ -42,31 +43,27 @@ describe("Suite test ZTag", () => {
   it("Test render z-tag with icon and text", async () => {
     const page = await newSpecPage({
       components: [ZTag],
-      html: `<z-tag class="body-5-sb" icon="gear" text="no progress"></z-tag>`,
+      html: `<z-tag class="body-5-sb" icon="gear">my custom element</z-tag>`,
     });
 
     expect(page.root).toEqualHtml(`
-      <z-tag class="body-5-sb" icon="gear" text="no progress">
-        <z-icon name="gear"></z-icon>
-        <span>
-          no progress
-        </span>
+      <z-tag class="body-5-sb" icon="gear" >
+        <z-icon name="gear" height="15" width="15"></z-icon>
+        <div>my custom element</div>
       </z-tag>
     `);
   });
 
-  it("Test render z-tag with icon and text with iconTextToken", async () => {
+  it("Test render z-tag with icon and text with Token", async () => {
     const page = await newSpecPage({
       components: [ZTag],
-      html: `<z-tag class="body-5-sb" icon="gear" text="no progress" style="--iconTextToken: var(--color-white); --bgToken: var(--avatar-C19);"></z-tag>`,
+      html: `<z-tag class="body-5-sb" icon="gear" style="--iconTextToken: var(--color-white); --bgToken: var(--avatar-C19);">my custom element</z-tag>`,
     });
 
     expect(page.root).toEqualHtml(`
-      <z-tag class="body-5-sb" icon="gear" text="no progress" style="--iconTextToken: var(--color-white); --bgToken: var(--avatar-C19);">
-        <z-icon name="gear"></z-icon>
-        <span>
-          no progress
-        </span>
+      <z-tag class="body-5-sb" icon="gear" style="--iconTextToken: var(--color-white); --bgToken: var(--avatar-C19);">
+        <z-icon name="gear" height="15" width="15"></z-icon>
+        <div>my custom element</div>
       </z-tag>
     `);
   });
