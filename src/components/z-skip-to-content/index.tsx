24303ee215c2b1bdee2373ec9443a6ad23efb0ff
Paolo F. Marino <p.marino@esis-italia.com>
2022-06-06 17:59:31 +0200

    feat: clean

--- src/components/z-skip-to-content/index.tsx
+++ src/components/z-skip-to-content/index.tsx
@@ -58,7 +58,6 @@ export class ZSkipToContent {
       e.preventDefault();
 
       if (prevElem) {
-        console.log(e.target);
         prevElem.classList.remove("link-visible");
         prevElem.classList.add("link-invisible");
         elem.classList.remove("link-invisible");
@@ -90,19 +89,3 @@ export class ZSkipToContent {
     );
   }
 }
-
-// var currentElement = $get(currentElementId); // ID set by OnFOcusIn
-// var curIndex = currentElement.tabIndex; //get current elements tab index
-// if (curIndex == lastTabIndex) {
-//   //if we are on the last tabindex, go back to the beginning
-//   curIndex = 0;
-// }
-// var tabbables = document.querySelectorAll(".tabable"); //get all tabable elements
-// for (var i = 0; i < tabbables.length; i++) {
-//   //loop through each element
-//   if (tabbables[i].tabIndex == curIndex + 1) {
-//     //check the tabindex to see if it's the element we want
-//     tabbables[i].focus(); //if it's the one we want, focus it and exit the loop
-//     break;
-//   }
-// }
