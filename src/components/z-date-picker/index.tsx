838583cf6208f7e656869d087e0c9b30f0957cb8
StefanoMarcolini97 <stefanomarcolini@MacBook-Pro-di-Stefano-2.local>
2022-06-23 09:32:49 +0200

    feat(z-date-picker-accessibility): add keyboard navigation to z-date-picker

--- src/components/z-date-picker/index.tsx
+++ src/components/z-date-picker/index.tsx
@@ -5,6 +5,7 @@ import {
   h,
   EventEmitter,
   Event,
+  Listen,
   State,
 } from "@stencil/core";
 
@@ -34,12 +35,48 @@ export class ZDatePicker {
 
   @State() flatpickrPosition: ZDatePickerPosition = ZDatePickerPosition.bottom;
 
+  private flatpickrInstance;
   private hasChildren: boolean;
 
   /** emitted when date changes, returns selected date */
   @Event() dateSelect: EventEmitter;
   emitDateSelect(date) {
     this.dateSelect.emit(date);
+  }
+
+  @Listen("keydown", { target: "body", capture: true })
+  handleKeyDown(ev: KeyboardEvent) {
+    let activeElement = document.activeElement;
+    console.log("activeElement", activeElement);
+
+    if (ev.key === "Enter") {
+      let isPrevArrowEntered = document.activeElement.classList.contains(
+        "flatpickr-prev-month"
+      );
+      let isNextArrowEntered = document.activeElement.classList.contains(
+        "flatpickr-next-month"
+      );
+      let prevArrow = this.element.querySelector(`.flatpickr-prev-month`);
+      let nextArrow = this.element.querySelector(`.flatpickr-next-month`);
+
+      if (this.mode === ZDatePickerMode.months) {
+        isPrevArrowEntered &&
+          this.flatpickrInstance.changeYear(
+            this.flatpickrInstance.currentYear - 1
+          );
+
+        isNextArrowEntered &&
+          this.flatpickrInstance.changeYear(
+            this.flatpickrInstance.currentYear + 1
+          );
+        isPrevArrowEntered && (prevArrow as HTMLElement).focus();
+        isNextArrowEntered && (nextArrow as HTMLElement).focus();
+      } else {
+        isPrevArrowEntered && this.flatpickrInstance.changeMonth(-1);
+        isNextArrowEntered && this.flatpickrInstance.changeMonth(1);
+      }
+    }
+    console.log(document.activeElement);
   }
 
   componentWillLoad() {
@@ -49,7 +86,7 @@ export class ZDatePicker {
   }
 
   componentDidLoad() {
-    flatpickr(`.${this.datepickerid}`, {
+    this.flatpickrInstance = flatpickr(`.${this.datepickerid}`, {
       appendTo: this.element.children[0] as HTMLElement,
       enableTime: this.mode === ZDatePickerMode.dateTime,
       locale: Italian,
@@ -60,6 +97,12 @@ export class ZDatePicker {
       time_24hr: true,
       onChange: (_selectedDates, dateStr) => {
         this.emitDateSelect(dateStr);
+      },
+      onOpen: () => {
+        this.setFlatpickrPosition();
+      },
+      onKeyDown: (_selectedDates, _dateStr, _instance, _event) => {
+        this.setTabindex();
       },
       wrap: this.hasChildren,
       plugins: this.mode === ZDatePickerMode.months && [
@@ -73,6 +116,34 @@ export class ZDatePicker {
     this.element.querySelectorAll(".flatpickr-weekday").forEach((element) => {
       element.innerHTML = element.innerHTML.trim().charAt(0);
     });
+  }
+
+  setTabindex() {
+    let calendars = this.element.getElementsByClassName("flatpickr-calendar");
+    let calendar = calendars[0];
+
+    //Set all flatpickr elements with tabindex=-1 to tabindex=0
+    let tabindexElements = calendar.querySelectorAll('[tabindex = "-1"]');
+    tabindexElements.forEach((element) =>
+      element.setAttribute("tabindex", "0")
+    );
+
+    //Set tabindex=0 to prev-month arrow and next-month arrow
+    calendar
+      .getElementsByClassName("flatpickr-prev-month")[0]
+      .setAttribute("tabindex", "0");
+    calendar
+      .getElementsByClassName("flatpickr-next-month")[0]
+      .setAttribute("tabindex", "0");
+
+    Array.from(calendar.getElementsByClassName("flatpickr-days")).forEach(
+      (element) => element.setAttribute("tabindex", "-1")
+    );
+
+    this.mode === ZDatePickerMode.months &&
+      Array.from(
+        calendar.getElementsByClassName("flatpickr-monthSelect-months")
+      ).forEach((element) => element.setAttribute("tabindex", "-1"));
   }
 
   setFlatpickrPosition() {
@@ -114,6 +185,7 @@ export class ZDatePicker {
         name="datepicker"
         icon="event"
         hasmessage={false}
+        tabindex="0"
       ></z-input>
     );
   }
@@ -127,7 +199,6 @@ export class ZDatePicker {
           this.flatpickrPosition,
           this.mode
         )}
-        onClick={() => this.setFlatpickrPosition()}
       >
         {this.hasChildren ? this.renderSlottedContent() : this.renderZInput()}
       </div>
