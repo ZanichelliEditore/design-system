1891844ddbded1ab14ff5fd9b78678216dc825ff
Dario <dariosacco9@gmail.com>
2022-08-25 13:37:39 +0200

    refactor: info reveal component

--- src/components/info-reveal/index.tsx
+++ src/components/info-reveal/index.tsx
@@ -5,6 +5,7 @@ import {
   State,
   Watch,
   Element,
+  Host,
 } from '@stencil/core';
 import { InfoRevealPosition } from "../../beans";
 
@@ -17,9 +18,11 @@ import { InfoRevealPosition } from "../../beans";
 /**
  * Info reveal component.
  *
+ * @slot - infos to display in the info box. If more than one element has been slotted, the infos can be navigated by clicking on them.
  * @cssprop --z-info-reveal-theme--surface - background color of the info reveal panel.
  * @cssprop --z-info-reveal-theme--text - foreground color of the info reveal panel.
  * @cssprop --z-info-reveal-shadow - shadow of the info reveal panel.
+ * @cssprop --z-info-reveal-max-width - max width of the info reveal panel. Defaults to 375px.
  */
 export class ZInfoReveal {
   @Element() el: HTMLElement;
@@ -37,54 +40,63 @@ export class ZInfoReveal {
   @State()
   open: boolean = false;
 
-  /** Current index for the statements queue. */
+  /** Current index for the info queue. */
   @State()
   currentIndex: number = null;
 
   @Watch("currentIndex")
   watchItems() {
-    Array.from(this.el.children).forEach((child, key) => {
-      if (this.currentIndex === key) {
-        child.classList.add('current');
+    Array.from(this.el.children).forEach((child, index) => {
+      if (this.currentIndex === index) {
+        child.setAttribute('data-current', '');
       } else {
-        child.classList.remove('current');
+        child.removeAttribute('data-current');
       }
     });
   }
 
-  isRightAligned() {
-    return this.position === InfoRevealPosition.BOTTOM_RIGHT ||
-      this.position === InfoRevealPosition.TOP_RIGHT;
+  /**
+   * Open the info box.
+   */
+  openInfoBox() {
+    this.currentIndex = 0;
+    this.open = true;
+  }
+
+  /**
+   * Close the info box.
+   */
+  closeInfoBox() {
+    this.open = false;
+  }
+
+  /**
+   * Navigate slotted infos.
+   * Close the info box after the last info has been navigated.
+   */
+  next() {
+    this.currentIndex = this.currentIndex + 1;
+    if (this.currentIndex === this.el.children.length) {
+      this.closeInfoBox();
+    }
   }
 
   render() {
-    return [
-      !this.open && <button class="closed-info-reveal" onClick={() => {
-        this.open = true;
-        this.currentIndex = 0;
-      }}>
-        {this.label && this.isRightAligned() && <span class="z-info-reveal-label">{this.label}</span>}
-        <z-icon fill="color-primary01" name={this.icon}></z-icon>
-        {this.label && !this.isRightAligned() && <span class="z-info-reveal-label">{this.label}</span>}
-      </button>,
-      this.open && [
-        <div
-          class="statements text-04"
-          onClick={() => {
-            this.currentIndex = this.currentIndex + 1;
-            if (this.currentIndex === this.el.children.length) {
-              this.open = false;
-              this.currentIndex = 0;
-            }
-          }}>
-          <slot></slot>
-          <button class="close" onClick={() => {
-            this.open = false;
-          }}>
-            <z-icon fill="color-primary01" name="close"></z-icon>
-          </button>
-        </div>,
-      ],
-    ];
+    return <Host open={this.open}>
+      <button class="z-info-reveal-trigger" onClick={this.openInfoBox.bind(this)}>
+        {this.label && <span class="z-info-reveal-label">{this.label}</span>}
+        <z-icon name={this.icon}></z-icon>
+      </button>
+      {this.open && <div
+        class="info-box"
+        onClick={this.next.bind(this)}
+        tabIndex={0}
+      >
+        <slot></slot>
+        <button class="z-info-reveal-close" onClick={this.closeInfoBox.bind(this)}>
+          <z-icon name="close"></z-icon>
+        </button>
+      </div>}
+    </Host>;
   }
 }
