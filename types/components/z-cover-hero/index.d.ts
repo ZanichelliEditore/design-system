6537beca3c84c382b0229afba4b3995b662a6233
DarioSacco <DarioSacco@users.noreply.github.com>
2023-01-23 14:07:07 +0000

    deploy: 78c27816be510e21edcd250fbc27ae90a8509372

--- types/components/z-cover-hero/index.d.ts
+++ types/components/z-cover-hero/index.d.ts
@@ -6,7 +6,8 @@ import { CoverHeroVariant, CoverHeroContentPosition } from "../../beans";
  * @slot cover - slot for the cover image.
  * @slot content - slot for the content.
  * @slot info-reveal - slot for the info reveal.
- * @cssprop --cover-hero-height - height of the cover hero.
+ * @cssprop --cover-hero-height - height of the cover hero. Default: `auto`.
+ * @cssprop --cover-hero-min-height - min height of the cover hero content. Default: 240px.
  * @cssprop --cover-hero-overlay - overlay color of the cover hero (CSS background property).
  * @cssprop --cover-hero-text-color - color of the text.
  */
