8dadd00949796e67029e6dff66d4cdece3b33835
Paolo F. Marino <p.marino@esis-ep.com>
2023-03-14 10:11:55 +0100

    feat: fix and added tests overlay variant and skipanimationonload prop

--- src/components/z-offcanvas/index.spec.ts
+++ src/components/z-offcanvas/index.spec.ts
@@ -2,7 +2,7 @@ import {newSpecPage} from "@stencil/core/testing";
 
 import {ZOffcanvas} from "./index";
 
-describe("Suite test ZOffcanvas push content", () => {
+describe("Suite test ZOffcanvas variant pushcontent", () => {
   it("Test render ZOffcanvas", async () => {
     const page = await newSpecPage({
       components: [ZOffcanvas],
@@ -13,12 +13,54 @@ describe("Suite test ZOffcanvas push content", () => {
 
     expect(page.root).toEqualHtml(`
 			<z-offcanvas open="" transitiondirection="left" variant="pushcontent">
-
 				<div class="canvas-container" role="presentation">
 					<div class="canvas-content" role="presentation">
 						<div slot="canvasContent"></div>
 					</div>
+        </div>
+      </z-offcanvas>
+    `);
+  });
+});
+
+describe("Suite test ZOffcanvas variant overlay", () => {
+  it("Test render ZOffcanvas", async () => {
+    const page = await newSpecPage({
+      components: [ZOffcanvas],
+      html: `<z-offcanvas open variant="overlay">
+              <div slot="canvasContent"></div>
+            </z-offcanvas>`,
+    });
+
+    expect(page.root).toEqualHtml(`
+			<z-offcanvas open="" transitiondirection="left" variant="overlay">
+				<div class="canvas-container" role="presentation">
+					<div class="canvas-content" role="presentation">
+						<div slot="canvasContent"></div>
 					</div>
+        </div>
+        <div class="canvas-background" data-action="canvasBackground"></div>
+      </z-offcanvas>
+    `);
+  });
+});
+
+describe("Suite test ZOffcanvas variant pushcontent with skipanimationonload", () => {
+  it("Test render ZOffcanvas", async () => {
+    const page = await newSpecPage({
+      components: [ZOffcanvas],
+      html: `<z-offcanvas open skipanimationonload>
+              <div slot="canvasContent"></div>
+            </z-offcanvas>`,
+    });
+
+    expect(page.root).toEqualHtml(`
+			<z-offcanvas open="" skipanimationonload="" transitiondirection="left" variant="pushcontent">
+				<div class="canvas-container" role="presentation">
+					<div class="canvas-content" role="presentation">
+						<div slot="canvasContent"></div>
+					</div>
+        </div>
       </z-offcanvas>
     `);
   });
