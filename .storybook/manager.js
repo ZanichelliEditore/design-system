ae44ce1431705ab906e4999a05fd961802447582
Dario <dariosacco9@gmail.com>
2023-03-23 12:50:20 +0100

    chore: remove unnecessary import

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,6 +1,5 @@
 import {addons} from "@storybook/addons";
 import theme from "./theme";
-import {STORY_CHANGED} from "@storybook/core-events";
 
 // just add the Zanichelli Design System theme
 addons.setConfig({
