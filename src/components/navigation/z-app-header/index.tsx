6bf123113bf5c69767b3227aebebfb2ae8657237
Dario <dariosacco9@gmail.com>
2023-05-03 16:56:16 +0200

    fix: app header hero check

--- src/components/navigation/z-app-header/index.tsx
+++ src/components/navigation/z-app-header/index.tsx
@@ -245,7 +245,7 @@ export class ZAppHeader {
    * Whether the header has a hero image, either as a prop or as a slot.
    */
   private get hasHero(): boolean {
-    return this.hero !== undefined || this.hostElement.querySelector("[slot=hero]") !== null;
+    return !!this.hero || this.hostElement.querySelector("[slot=hero]") !== null;
   }
 
   private openDrawer(): void {
