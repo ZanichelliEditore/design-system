c306204b979006d2ffdca9ab2f7ffcff5494d0ff
AleZuddas <a.zuddas@esis-italia.com>
2023-09-06 15:55:24 +0200

    chore(LAZE-556): changed spacer class name

--- src/components/modal/z-modal/index.tsx
+++ src/components/modal/z-modal/index.tsx
@@ -200,7 +200,7 @@ export class ZModal {
           >
             <slot name="modalContent"></slot>
           </div>
-          {!this.scrollable && <div class="spacer"></div>}
+          {!this.scrollable && <div class="modal-spacer"></div>}
         </div>
         <div
           class={{
