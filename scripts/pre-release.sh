b1ff010c3d3d17f42094289c9441b617dc24a5a7
FrancescoSanti <fsanti@zanichelli.it>
2024-03-14 14:08:04 +0100

    feat: restore rc job, add release notes step to latest

--- scripts/pre-release.sh
+++ scripts/pre-release.sh
@@ -5,4 +5,4 @@ git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${GITHUB_ACTOR_ID}+${GITHUB_ACTOR}@users.noreply.github.com"
 yarn
 yarn build
-# yarn test
+yarn test
--- scripts/release-notes.sh
+++ scripts/release-notes.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 VERSION_NUMBER=`cat package.json | grep "\"version\": " | grep -o "[0-9]*\.[0-9]*\.[0-9]*"`
-gh release create v$VERSION_NUMBER --draft --generate-notes
+gh release create v$VERSION_NUMBER --latest --generate-notes
