7d8dd12e5483f942887d7168369b4dc8919e03f8
FrancescoSanti <fsanti@zanichelli.it>
2024-05-02 13:02:23 +0200

    feat: add wip test on z-button-sort

--- src/components/buttons/z-button-sort/test.e2e.ts
+++ src/components/buttons/z-button-sort/test.e2e.ts
@@ -13,3 +13,21 @@ it("Test ZButtonSort should emit buttonSortClick event", async () => {
   await page.waitForChanges();
   expect(clickEvent).toHaveReceivedEventDetail({sortAsc: false});
 });
+
+it("changes button label on click based on sortAsc event property", async () => {
+  const page = await newE2EPage({
+    html: '<z-button-sort label="ascending" desclabel="descending" sortasc="true"></z-button-sort>',
+  });
+  const btn = await page.find("z-button-sort >>> button");
+  const clickEvent = await page.spyOnEvent("buttonSortClick");
+  const btnText = await page.find("z-button-sort >>> button span.ellipsis");
+  // manca da testare il cambio di label
+
+  await btn.click();
+  await page.waitForChanges();
+  expect(clickEvent).toHaveReceivedEventDetail({sortAsc: true});
+
+  await btn.click();
+  await page.waitForChanges();
+  expect(clickEvent).toHaveReceivedEventDetail({sortAsc: false});
+});
