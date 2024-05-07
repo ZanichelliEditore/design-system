5110560ee7118a886f6bf54435dd3e34dca35e1a
FrancescoSanti <fsanti@zanichelli.it>
2024-05-07 16:48:21 +0200

    feat: test button label change on click

--- src/components/buttons/z-button-sort/test.e2e.ts
+++ src/components/buttons/z-button-sort/test.e2e.ts
@@ -16,18 +16,19 @@ it("Test ZButtonSort should emit buttonSortClick event", async () => {
 
 it("changes button label on click based on sortAsc event property", async () => {
   const page = await newE2EPage({
-    html: '<z-button-sort label="ascending" desclabel="descending" sortasc="true"></z-button-sort>',
+    html: '<z-button-sort label="ascending" desclabel="descending" sortasc="true" isselected="true"></z-button-sort>',
   });
   const btn = await page.find("z-button-sort >>> button");
   const clickEvent = await page.spyOnEvent("buttonSortClick");
   const btnText = await page.find("z-button-sort >>> button span.ellipsis");
-  // manca da testare il cambio di label
-
-  await btn.click();
-  await page.waitForChanges();
-  expect(clickEvent).toHaveReceivedEventDetail({sortAsc: true});
 
   await btn.click();
   await page.waitForChanges();
   expect(clickEvent).toHaveReceivedEventDetail({sortAsc: false});
+  expect(btnText).toEqualText("descending");
+
+  await btn.click();
+  await page.waitForChanges();
+  expect(clickEvent).toHaveReceivedEventDetail({sortAsc: true});
+  expect(btnText).toEqualText("ascending");
 });
