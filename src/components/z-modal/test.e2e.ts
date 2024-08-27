7832eac1a4462661d98d0f927d96393e89e46153
FrancescoSanti <fsanti@zanichelli.it>
2024-08-27 15:22:05 +0200

    fix: add tests on modal close

---
+++ src/components/z-modal/test.e2e.ts
@@ -0,0 +1,46 @@
+import {newE2EPage} from "@stencil/core/testing";
+
+describe("z-modal test end2end", () => {
+  it("Close modal using x button", async () => {
+    const page = await newE2EPage();
+    await page.setContent(`
+    <div>
+      <z-modal modalid="my-modal-short" modaltitle="My modal title" close-button-label="chiudi modale" alertdialog="false" closable="true" scroll-inside="true">
+        <div slot="modalContent">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </div>
+      </z-modal>
+    </div>`);
+    const modalCloseEvent = await page.spyOnEvent("modalClose");
+    const closeButton = await page.find("z-modal >>> header button");
+    const modal = await page.find("z-modal >>> dialog");
+
+    await closeButton.click();
+    await page.waitForChanges();
+
+    expect(modalCloseEvent).toHaveReceivedEvent();
+    await modal.waitForNotVisible();
+  });
+
+  it("Close modal clicking outside dialog", async () => {
+    const page = await newE2EPage();
+    await page.setContent(`
+    <div>
+      <input class="other-element" />
+      <z-modal modalid="my-modal-short" modaltitle="My modal title" close-button-label="chiudi modale" alertdialog="false" closable="true" scroll-inside="true">
+        <div slot="modalContent">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </div>
+      </z-modal>
+    </div>`);
+    const modalCloseEvent = await page.spyOnEvent("modalClose");
+    const input = await page.find(".other-element");
+    const modal = await page.find("z-modal >>> dialog");
+
+    await input.click();
+    await page.waitForChanges();
+
+    expect(modalCloseEvent).toHaveReceivedEvent();
+    await modal.waitForNotVisible();
+  });
+});
