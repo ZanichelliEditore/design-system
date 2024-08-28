e5308fc924308f7a1c2f26076ca93488797d9011
Paolo F. Marino <pmarino@zanichelli.it>
2024-08-28 18:01:44 +0200

    feat: fix focus header

--- src/components/z-app-header/index.tsx
+++ src/components/z-app-header/index.tsx
@@ -434,17 +434,16 @@ export class ZAppHeader {
     return Array.from(this.menuElements).reduce((acc, item) => item.getBoundingClientRect().width + acc, 0);
   }
 
-  private focusToFirstItemMenu(e): void {
-    if (e.shiftKey && e.code === KeyboardCode.TAB && this.currentIndex === 0) {
-      const input = this.searchbar.shadowRoot.querySelector("z-input input") as HTMLInputElement;
-      input.focus();
-      this.currentIndex = -1;
-    } else if (this.currentIndex === -1) {
-      const menuItems = Array.from(this.menuElements).map(
-        (el) => el.shadowRoot.querySelector(".menu-label") as HTMLElement
-      );
+  private focusToFirstItemMenu(): void {
+    const menuItems = Array.from(this.menuElements).map(
+      (el) => el.shadowRoot.querySelector(".menu-label") as HTMLElement
+    );
+
+    if (this.currentIndex === -1) {
       menuItems[0].focus();
       this.currentIndex = 0;
+    } else if (this.currentIndex !== -1) {
+      menuItems[this.currentIndex].focus();
     }
   }
 
