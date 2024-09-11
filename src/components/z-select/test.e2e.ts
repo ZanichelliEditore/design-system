762b0982ded4045a12fe66c9340f52b8462c8077
FrancescoSanti <fsanti@zanichelli.it>
2024-09-11 16:41:13 +0200

    fix: test that the select item closes when clicking outside

--- src/components/z-select/test.e2e.ts
+++ src/components/z-select/test.e2e.ts
@@ -14,7 +14,7 @@ describe("z-select test end2end", () => {
     await page.locator("z-select").click();
     await page.waitForChanges();
 
-    expect((await page.find("z-select input")).getAttribute("aria-expanded")).toBeTruthy;
+    expect((await page.find("z-select input")).getAttribute("aria-expanded")).toBe("true");
   });
 
   it("Should select an item from the list and then close the select list", async () => {
@@ -34,7 +34,31 @@ describe("z-select test end2end", () => {
     await page.locator("z-select z-list-element").click();
     await page.waitForChanges();
 
-    expect((await page.find("z-select input")).getAttribute("aria-expanded")).toBeFalsy;
+    expect((await page.find("z-select input")).getAttribute("aria-expanded")).toBe("false");
     expect(optionSelectEvent).toHaveReceivedEvent();
+  });
+
+  it("Should close the select list when clicking outside the component", async () => {
+    const page = await newE2EPage();
+
+    await page.setContent(`
+      <div>
+        <z-input class="outside-element"></z-input>
+        <z-select
+          items='[{"id":"item_1","selected":false,"name":"item_1"},{"id":"item_2","selected":true,"name":"item_2"},{"id":"item_3","selected":false,"name":"item_3"}]'
+          label="this is the label"
+        ></z-select>
+      </div>
+    `);
+
+    await page.locator("z-select").click();
+    await page.waitForChanges();
+
+    expect((await page.find("z-select input")).getAttribute("aria-expanded")).toBe("true");
+
+    await page.locator(".outside-element").click();
+    await page.waitForChanges();
+
+    expect((await page.find("z-select input")).getAttribute("aria-expanded")).toBe("false");
   });
 });
