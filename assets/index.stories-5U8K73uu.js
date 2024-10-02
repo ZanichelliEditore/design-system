602635ae5435d67e9ba80d42ef013122fb32b53d
DarioSacco <DarioSacco@users.noreply.github.com>
2024-10-02 08:01:42 +0000

    deploy: ce8dfa715953700506af66e387a413df4b9dc842

---
+++ assets/index.stories-5U8K73uu.js
@@ -0,0 +1,45 @@
+import{x as a}from"./lit-element-DGXdXVoE.js";import{i as d}from"./index-CjlG6izv.js";import{a as p}from"./storybook-utils-DAVCxM-F.js";import"./index-CzJSQeMW.js";import"./index-s0dGocgi.js";import"./chunk-L4EGOTBX-lo-pZyle.js";import"./entry-preview-Cx7Tr7Nx.js";import"./index-DrFu-skq.js";import"./index-BZ0rSFNp.js";const $={title:"ZSectionTitle",component:"z-section-title",argTypes:{dividerPosition:{options:Object.values(d),control:{type:"inline-radio"}},"--z-section-title--divider-color":p()},args:{uppercase:!0,"--z-section-title--divider-color":"var(--red500)"}},i={args:{dividerPosition:d.BEFORE},render:e=>a`
+    <z-section-title
+      .dividerPosition=${e.dividerPosition}
+      .uppercase=${e.uppercase}
+      .style="--z-section-title--divider-color: ${e["--z-section-title--divider-color"]}"
+    >
+      <h4 slot="primary-title">Primary title</h4>
+    </z-section-title>
+  `},t={parameters:{controls:{exclude:["dividerPosition"]}},render:e=>a`
+    <z-section-title
+      .uppercase=${e.uppercase}
+      .style="--z-section-title--divider-color: ${e["--z-section-title--divider-color"]}"
+    >
+      <h5 slot="secondary-title">Secondary title</h5>
+      <h4 slot="primary-title">Primary title</h4>
+    </z-section-title>
+  `};var r,o,s;i.parameters={...i.parameters,docs:{...(r=i.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    dividerPosition: ZSectionTitleDividerPosition.BEFORE
+  },
+  render: args => html\`
+    <z-section-title
+      .dividerPosition=\${args.dividerPosition}
+      .uppercase=\${args.uppercase}
+      .style="--z-section-title--divider-color: \${args["--z-section-title--divider-color"]}"
+    >
+      <h4 slot="primary-title">Primary title</h4>
+    </z-section-title>
+  \`
+} satisfies Story`,...(s=(o=i.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};var n,l,c;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  parameters: {
+    controls: {
+      exclude: ["dividerPosition"]
+    }
+  },
+  render: args => html\`
+    <z-section-title
+      .uppercase=\${args.uppercase}
+      .style="--z-section-title--divider-color: \${args["--z-section-title--divider-color"]}"
+    >
+      <h5 slot="secondary-title">Secondary title</h5>
+      <h4 slot="primary-title">Primary title</h4>
+    </z-section-title>
+  \`
+} satisfies Story`,...(c=(l=t.parameters)==null?void 0:l.docs)==null?void 0:c.source}}};const T=["PrimaryTitleOnly","SecondaryTitle"];export{i as PrimaryTitleOnly,t as SecondaryTitle,T as __namedExportsOrder,$ as default};
---
+++ assets/index.stories-DYmetgqu.js
@@ -0,0 +1,13 @@
+import{x as i}from"./lit-element-DGXdXVoE.js";import"./index-DCNgr5r_.js";import"./chunk-L4EGOTBX-lo-pZyle.js";import"./entry-preview-Cx7Tr7Nx.js";import"./index-DrFu-skq.js";import"./index-BZ0rSFNp.js";const g={title:"Snowflakes/MyzCard/ZMyzCardCover",component:"z-myz-card-cover",args:{defaultimg:"https://staticmy.zanichelli.it/catalogo/assets/m40001.9788808720740.jpg",faded:!1,img:"https://staticmy.zanichelli.it/catalogo/assets/m40001.9788808720740.jpg",titolo:"Title"}},e={render:t=>i`<z-myz-card-cover
+      defaultimg=${t.defaultimg}
+      faded=${t.faded}
+      img=${t.img}
+      titolo=${t.titolo}
+    />`};var a,r,o;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: args => html\`<z-myz-card-cover
+      defaultimg=\${args.defaultimg}
+      faded=\${args.faded}
+      img=\${args.img}
+      titolo=\${args.titolo}
+    />\`
+}`,...(o=(r=e.parameters)==null?void 0:r.docs)==null?void 0:o.source}}};const p=["ZMyzCardCover"];export{e as ZMyzCardCover,p as __namedExportsOrder,g as default};
