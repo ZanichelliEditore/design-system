41d52f909ebe102e57f5e11665bb0036775ecfad
paolosantarsiero <paolosantarsiero9@gmail.com>
2024-10-07 10:43:33 +0200

    feat: add e2e test for z-info-reveal

---
+++ src/components/z-info-reveal/test.e2e.ts
@@ -0,0 +1,53 @@
+import {newE2EPage} from "@stencil/core/testing";
+
+describe("z-info-reveal test end2end", () => {
+  it("Open", async () => {
+    const page = await newE2EPage({
+      html: "<z-info-reveal><div>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div></z-info-reveal>",
+    });
+
+    const infoRevealPanel = await page.find("z-info-reveal >>> .z-info-reveal-panel");
+    const infoRevealTrigger = await page.find("z-info-reveal >>> .z-info-reveal-trigger");
+
+    await infoRevealTrigger.click();
+    await page.waitForChanges();
+    await infoRevealPanel.waitForVisible();
+  });
+
+  it("Close info reveal using x button", async () => {
+    const page = await newE2EPage({
+      html: "<z-info-reveal><div>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div></z-info-reveal>",
+    });
+
+    const infoRevealPanel = await page.find("z-info-reveal >>> .z-info-reveal-panel");
+    const infoRevealTrigger = await page.find("z-info-reveal >>> .z-info-reveal-trigger");
+
+    await infoRevealTrigger.click();
+    await page.waitForChanges();
+    await infoRevealPanel.waitForVisible();
+
+    const closeButton = await page.find("z-info-reveal >>> .z-info-reveal-close");
+    await closeButton.click();
+    await page.waitForChanges();
+
+    await infoRevealPanel.waitForNotVisible();
+  });
+
+  it("Close with keydown press", async () => {
+    const page = await newE2EPage({
+      html: "<z-info-reveal><div>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div></z-info-reveal>",
+    });
+
+    const infoRevealPanel = await page.find("z-info-reveal >>> .z-info-reveal-panel");
+    const infoRevealTrigger = await page.find("z-info-reveal >>> .z-info-reveal-trigger");
+
+    await infoRevealTrigger.click();
+    await page.waitForChanges();
+    await infoRevealPanel.waitForVisible();
+
+    await page.keyboard.press("Escape");
+    await page.waitForChanges();
+
+    await infoRevealPanel.waitForNotVisible();
+  });
+});
