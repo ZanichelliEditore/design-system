69be23383ef77d9c23e2d3841efb6fa6e31ab4d1
DarioSacco <DarioSacco@users.noreply.github.com>
2024-10-09 08:06:47 +0000

    deploy: 8602292f4698197f2909bae0f1fe6617b4bb425d

---
+++ assets/index.stories-BDzx5aVV.js
@@ -0,0 +1,101 @@
+import{x as $}from"./lit-element-DGXdXVoE.js";import{E as l,L as t,D as n,a as o,b as s,c as d}from"./index-CjlG6izv.js";import{g as a}from"./storybook-utils-DAVCxM-F.js";import"./index-Cpg4iddH.js";import"./index-DepmNu5m.js";import"./chunk-L4EGOTBX-lo-pZyle.js";import"./entry-preview-Cx7Tr7Nx.js";import"./index-DrFu-skq.js";import"./index-BZ0rSFNp.js";import"./index-DG5VvcyV.js";import"./index-BmyQjKNB.js";const k={title:"ZList/ZListElement",component:"z-list-element",argTypes:{alignButton:{control:{type:"inline-radio"},options:Object.values(l)},dividerColor:{control:{type:"select"},options:a().map(e=>e.replace("--",""))},dividerType:{control:{type:"inline-radio"},options:Object.values(t)},dividerSize:{control:{type:"inline-radio"},options:Object.values(n)},expandableStyle:{control:{type:"inline-radio"},options:Object.values(o)},size:{control:{type:"inline-radio"},options:Object.values(s)},color:{control:{type:"select"},options:a().map(e=>e.replace("--",""))},listType:{control:{type:"inline-radio"},options:Object.values(d)}},args:{alignButton:l.LEFT,clickable:!1,dividerColor:"color-surface03",dividerType:t.NONE,dividerSize:n.SMALL,expandable:!1,expandableStyle:o.ACCORDION,listElementId:0,size:s.MEDIUM,color:"color-default-text",disabled:!0,listElementPosition:"0",listType:d.NONE}},i={render:e=>$`<z-list>
+      <z-list-element
+        align-button=${e.alignButton}
+        clickable=${e.clickable}
+        divider-color=${e.dividerColor}
+        divider-type=${e.dividerType}
+        divider-size=${e.dividerSize}
+        expandable=${e.expandable}
+        expandable-style=${e.expandableStyle}
+        list-element-id=${e.listElementId}
+        size=${e.size}
+        color=${e.color}
+        disabled=${e.disabled}
+        list-element-position=${e.listElementPosition}
+        list-type=${e.listType}
+        ><span>Elemento 1</span><span slot="inner-content">Inner content</span></z-list-element
+      >
+      <z-list-element
+        align-button=${e.alignButton}
+        clickable=${e.clickable}
+        divider-color=${e.dividerColor}
+        divider-type=${e.dividerType}
+        divider-size=${e.dividerSize}
+        expandable=${e.expandable}
+        expandable-style=${e.expandableStyle}
+        list-element-id=${e.listElementId}
+        size=${e.size}
+        color=${e.color}
+        disabled=${e.disabled}
+        list-element-position=${e.listElementPosition}
+        list-type=${e.listType}
+        >Elemento 2</z-list-element
+      >
+      <z-list-element
+        align-button=${e.alignButton}
+        clickable=${e.clickable}
+        divider-color=${e.dividerColor}
+        divider-type=${e.dividerType}
+        divider-size=${e.dividerSize}
+        expandable=${e.expandable}
+        expandable-style=${e.expandableStyle}
+        list-element-id=${e.listElementId}
+        size=${e.size}
+        color=${e.color}
+        disabled=${e.disabled}
+        list-element-position=${e.listElementPosition}
+        list-type=${e.listType}
+        >Elemento 3</z-list-element
+      >
+    </z-list>`};var r,p,c;i.parameters={...i.parameters,docs:{...(r=i.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: args => html\`<z-list>
+      <z-list-element
+        align-button=\${args.alignButton}
+        clickable=\${args.clickable}
+        divider-color=\${args.dividerColor}
+        divider-type=\${args.dividerType}
+        divider-size=\${args.dividerSize}
+        expandable=\${args.expandable}
+        expandable-style=\${args.expandableStyle}
+        list-element-id=\${args.listElementId}
+        size=\${args.size}
+        color=\${args.color}
+        disabled=\${args.disabled}
+        list-element-position=\${args.listElementPosition}
+        list-type=\${args.listType}
+        ><span>Elemento 1</span><span slot="inner-content">Inner content</span></z-list-element
+      >
+      <z-list-element
+        align-button=\${args.alignButton}
+        clickable=\${args.clickable}
+        divider-color=\${args.dividerColor}
+        divider-type=\${args.dividerType}
+        divider-size=\${args.dividerSize}
+        expandable=\${args.expandable}
+        expandable-style=\${args.expandableStyle}
+        list-element-id=\${args.listElementId}
+        size=\${args.size}
+        color=\${args.color}
+        disabled=\${args.disabled}
+        list-element-position=\${args.listElementPosition}
+        list-type=\${args.listType}
+        >Elemento 2</z-list-element
+      >
+      <z-list-element
+        align-button=\${args.alignButton}
+        clickable=\${args.clickable}
+        divider-color=\${args.dividerColor}
+        divider-type=\${args.dividerType}
+        divider-size=\${args.dividerSize}
+        expandable=\${args.expandable}
+        expandable-style=\${args.expandableStyle}
+        list-element-id=\${args.listElementId}
+        size=\${args.size}
+        color=\${args.color}
+        disabled=\${args.disabled}
+        list-element-position=\${args.listElementPosition}
+        list-type=\${args.listType}
+        >Elemento 3</z-list-element
+      >
+    </z-list>\`
+} satisfies StoryObj<ZListElement>`,...(c=(p=i.parameters)==null?void 0:p.docs)==null?void 0:c.source}}};const L=["Default"];export{i as Default,L as __namedExportsOrder,k as default};
