dae84093e50903d0ef4522955c1fe675eedfe360
Veronica Vannini <vvannini@zanichelli.it>
2024-11-06 17:48:57 +0100

    chore: add click on menu item test

--- src/components/z-menu/test.e2e.ts
+++ src/components/z-menu/test.e2e.ts
@@ -28,4 +28,31 @@ describe("z-menu test end2end", () => {
     await page.waitForChanges();
     expect(closed).toHaveReceivedEvent();
   });
+
+  it("Click on menu item", async () => {
+    const page = await newE2EPage();
+    await page.setContent(`
+			<z-menu>
+        <h3>Menu label</h3>
+        <a href="https://www.google.it/" slot="item">Item 1</a>
+        <a href="" slot="item">Item 2</a>
+      </z-menu>
+		`);
+
+    const menu = await page.find("z-menu");
+    const menuOpen = await page.find("z-menu[open]");
+    expect(menu).not.toBeNull();
+    expect(menuOpen).toBeNull();
+
+    await menu.click();
+
+    expect(await page.$eval("z-menu >>> button", (e) => getComputedStyle(e).borderColor)).toBe("rgb(0, 86, 193)");
+
+    const menuItem = await page.find('z-menu a[href="https://www.google.it/"]');
+    expect(menuItem).not.toBeNull();
+
+    await menuItem.click();
+    await page.waitForChanges();
+    expect(page.url()).toBe("https://www.google.it/");
+  });
 });
