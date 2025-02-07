2fedb2f2b0fe0cd6e98a0772db9b8f1494e7324c
Alessandro Zuddas <a.zuddas@esis-italia.com>
2025-02-07 09:54:19 +0100

    chore: fix sizes of groupedTree divider

--- src/components/z-select/index.tsx
+++ src/components/z-select/index.tsx
@@ -1,5 +1,5 @@
 import {Component, Element, Event, EventEmitter, Listen, Method, Prop, State, Watch, h} from "@stencil/core";
-import {ControlSize, InputStatus, KeyboardCode, ListDividerType, ListSize, SelectItem} from "../../beans";
+import {ControlSize, DividerSize, InputStatus, KeyboardCode, ListDividerType, ListSize, SelectItem} from "../../beans";
 import {boolean, getClickedElement, getElementTree, handleKeyboardSubmit, randomId} from "../../utils/utils";
 
 @Component({
@@ -764,11 +764,17 @@ export class ZSelect {
                 <z-divider
                   key={`divider-${i}`}
                   style={{zIndex: "100"}}
+                  size={DividerSize.SMALL}
                 />
               ) : null,
             ])}
           </z-list>
-          {index !== entries.length - 1 && <z-divider style={{zIndex: "100"}} />}
+          {index !== entries.length - 1 && (
+            <z-divider
+              style={{zIndex: "100"}}
+              size={DividerSize.SMALL}
+            />
+          )}
         </z-list-group>
       );
     });
