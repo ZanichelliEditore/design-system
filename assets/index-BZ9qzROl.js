55ad30445a2c6c2d35cc17c315c058ee41f6f25b
LorenzoEmanuele00 <LorenzoEmanuele00@users.noreply.github.com>
2025-03-18 10:03:38 +0000

    deploy: 3d151c075db772efcf32d97048e3b2cb7d0e322f

--- assets/index-BZ9qzROl.js
+++
@@ -1,1 +0,0 @@
-import{M,u as a}from"./index-DKTm-tT8.js";import"./index-DQJX0yTZ.js";import"./iframe-CX0GTaPo.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-BcT0yLYp.js";import"./index-DrFu-skq.js";export{M as MDXProvider,a as useMDXComponents};
---
+++ assets/index.stories-BW6XluFz.js
@@ -0,0 +1,195 @@
+import{x as o}from"./lit-element-DGXdXVoE.js";import"./index-yAa2HCv5.js";import"./index-kyZRTsWd.js";import"./index-C7uKMLhB.js";import"./index-BP-rsxYr.js";import"./index-C9VWc8lD.js";import"./index-27H_-3FH.js";import{s as u,g as r}from"./chunk-L4EGOTBX-lo-pZyle.js";import"./entry-preview-Cx7Tr7Nx.js";import{d as h,H as v,c as g,h as a}from"./index-BZ0rSFNp.js";import{B as $,t as k}from"./index-Dm3VcLdc.js";import"./index-BSoUoz93.js";import"./index-CHvo4J7Y.js";import"./utils-WOnXK32B.js";import"./breakpoints-p3CVLX0Q.js";import"./index-DrFu-skq.js";const L=`:host{font-family:var(--font-family-sans);font-weight:var(--font-rg)}:host z-myz-card{--card-overflow:visible}:host>div{position:relative;width:100%;height:522px;transition:-webkit-transform 0.5s ease-out;transition:transform 0.5s ease-out;transition:transform 0.5s ease-out,
+    -webkit-transform 0.5s ease-out}:host>div .content{position:relative;z-index:2;width:100%;height:522px;transform-style:preserve-3d;transition:-webkit-transform 0.5s ease-out;transition:transform 0.5s ease-out;transition:transform 0.5s ease-out,
+    -webkit-transform 0.5s ease-out}:host>div .content.flipped{margin-top:-2px;transform:rotateY(180deg)}:host>div .front,:host>div .back{position:absolute;top:0;left:0;width:100%;height:100%;backface-visibility:hidden}:host>div .front{z-index:2;top:0;left:0;transform:rotateY(0deg)}:host>div .front z-button{position:absolute;z-index:3;top:calc(var(--space-unit) * 0.5);left:calc(-1 * var(--space-unit))}:host>div .back{z-index:1;top:0;left:0;display:flex;justify-content:center;padding:0 var(--space-unit);margin-left:calc(-1 * var(--space-unit));transform:rotateY(180deg)}.hide-info{display:none}`,C=class extends v{constructor(){super(),this.__registerHost(),this.__attachShadow(),this.cardFlipped=g(this,"cardFlipped",7),this.name=void 0,this.cover=void 0,this.disabled=!1,this.flipped=!1,this.flipbuttonlabel="INFO",this.hideinfobtn=!1}handleFlipCard(i){this.flipCard(i.detail)}flipCard(i=!0){this.flipped=i,this.cardFlipped.emit(i)}render(){return a("div",{key:"47f7c11898713dea32e9733836385c55ebb6ce62"},a("z-myz-card",{key:"ff3431af8dd58f91e57b332815d9cb928c08478b"},a("z-myz-card-header",{key:"feb6e0db37be57ab4295af1bed7b44195312bfec",titolo:this.name}),a("div",{key:"1e22f9576e5fcff71be9087cacdf06d3e7894d7f",class:`content ${this.flipped?"flipped":""}`},a("div",{key:"b730815455ae26d614167ad44608450383d269f9",class:"front"},a("z-myz-card-body",{key:"6011c28198253938a9dbe6d2cae703f500920257"},a("z-myz-card-cover",{key:"9a7b49f84060adb68d2caa64d782daa6c30c9c2e",slot:"cover",titolo:this.name,img:this.cover,faded:this.disabled})),a("z-button",{key:"282b9ad55d7de9697d32dc058268dc64ad6b8e3b",class:{"hide-info":this.hideinfobtn},variant:$.SECONDARY,icon:"informationsource",size:k.X_SMALL,onClick:()=>this.flipCard(!0),disabled:this.flipped},this.flipbuttonlabel)),a("div",{key:"5444c315bb9967908cd123aa2f5987e1f0c2f8c0",class:"back"},a("slot",{key:"61b5dcc792f5387a1cc095f240550433973098c3",name:"info"}))),a("slot",{key:"6add468bee0dc1142f614d72e70aa14ed4f73696"})))}static get style(){return L}};h(C,[1,"z-myz-card-dictionary",{name:[1],cover:[1],disabled:[4],flipped:[1028],flipbuttonlabel:[1],hideinfobtn:[4]},[[0,"flipCard","handleFlipCard"]]]);const d={schemaVersion:"1.0.0",modules:[{kind:"javascript-module",path:"",declarations:[{kind:"class",name:"ZMyzCardDictionary",tagName:"z-myz-card-dictionary",customElement:!0,members:[{kind:"field",name:"name",type:"string",description:"card title"},{kind:"field",name:"cover",type:"string",description:"card cover"},{kind:"field",name:"disabled",type:"boolean",description:"card is disabled",default:"false"},{kind:"field",name:"flipped",type:"boolean",description:"card is flipped",default:"false"},{kind:"field",name:"flipbuttonlabel",type:"string",description:"flip button label",default:'"INFO"'},{kind:"field",name:"hideinfobtn",type:"boolean",description:"hide info button",default:"false"}],events:[{kind:"event",name:"cardFlipped",description:"when card is flipped"}],slots:[{name:"-",description:"generic card slot"},{name:"info",description:"flipped card info"}]}],exports:[{kind:"js",name:"ZMyzCardDictionary",declaration:{name:"ZMyzCardDictionary",module:"src/snowflakes/myz/card/z-myz-card-dictionary/index.tsx"}},{kind:"custom-element-definition",name:"z-myz-card-dictionary",declaration:{name:"ZMyzCardDictionary",module:"src/snowflakes/myz/card/z-myz-card-dictionary/index.tsx"}}]}]};u({...r()||{},...d,modules:[...(r()||{}).modules||[],...d.modules]});const R={title:"Snowflakes/MyzCard/ZMyzCardDictionary",component:"z-myz-card-dictionary",args:{cover:"https://staticmy.zanichelli.it/catalogo/assets/m40004.9788808194121.jpg",disabled:!1,hideinfobtn:!1,flipped:!1,flipbuttonlabel:"info"}},t={render:i=>o`
+    <z-myz-card-dictionary
+      name="${i.name}"
+      cover="${i.cover}"
+      disabled="${i.disabled}"
+      hideinfobtn="${i.hideinfobtn}"
+      flipped="${i.flipped}"
+      flipbuttonlabel="${i.flipbuttonlabel}"
+    >
+      <z-myz-card-footer-sections>
+        <div slot="top">Vocabolario della lingua italiana</div>
+        <div slot="bottom">
+          <z-button
+            variant="primary"
+            style="width:100%;"
+            >VAI AL PRODOTTO</z-button
+          >
+        </div>
+      </z-myz-card-footer-sections>
+      <z-myz-card-info
+        slot="info"
+        data='{"author":"Nicola Zingarelli","year":"2021","title":"Vocabolario della lingua italiana","description":"A cura di Mario Cannella, Beata Lazzarini","onlineLicense":{"expiration":"31/12/2021","installations":"1"},"offlineLicense":{"expiration":"30/06/2021","installations":"2"}}'
+      >
+        <z-button
+          variant="tertiary"
+          icon="gear"
+          >Gestisci Licenze</z-button
+        >
+      </z-myz-card-info>
+    </z-myz-card-dictionary>
+  `},n={render:i=>o`
+    <z-myz-card-dictionary
+      name="${i.name}"
+      cover="${i.cover}"
+      disabled="${i.disabled}"
+      hideinfobtn="${i.hideinfobtn}"
+      flipped="${i.flipped}"
+      flipbuttonlabel="${i.flipbuttonlabel}"
+    >
+      <z-myz-card-footer-sections>
+        <div slot="top">Vocabolario della lingua italiana</div>
+        <div slot="bottom">
+          <z-button
+            variant="primary"
+            style="width:100%;"
+            >VAI AL PRODOTTO</z-button
+          >
+        </div>
+      </z-myz-card-footer-sections>
+      <z-myz-card-info
+        slot="info"
+        data='{"author":"Nicola Zingarelli","year":"2021","title":"Vocabolario della lingua italiana","description":"A cura di Mario Cannella, Beata Lazzarini","onlineLicense":{"expiration":"31/12/2021","installations":"1"},"offlineLicense":{"expiration":"30/06/2021","installations":"2"}}'
+      >
+        <z-button
+          variant="tertiary"
+          icon="gear"
+          >Gestisci Licenze</z-button
+        >
+      </z-myz-card-info>
+    </z-myz-card-dictionary>
+  `,args:{flipped:!0}},e={render:i=>o`
+    <z-myz-card-dictionary
+      name="${i.name}"
+      cover="${i.cover}"
+      disabled="${i.disabled}"
+      hideinfobtn="${i.hideinfobtn}"
+      flipped="${i.flipped}"
+      flipbuttonlabel="${i.flipbuttonlabel}"
+    >
+      <z-myz-card-footer-sections>
+        <div slot="top">Vocabolario della lingua italiana</div>
+        <div slot="bottom">
+          <z-button
+            variant="primary"
+            style="width:100%;"
+            >VAI AL PRODOTTO</z-button
+          >
+        </div>
+      </z-myz-card-footer-sections>
+      <z-myz-card-info
+        slot="info"
+        data='{"author":"Nicola Zingarelli","year":"2021","title":"Vocabolario della lingua italiana","description":"A cura di Mario Cannella, Beata Lazzarini","onlineLicense":{"expiration":"31/12/2021","installations":"1"},"offlineLicense":{"expiration":"30/06/2021","installations":"2"}}'
+      >
+        <z-button
+          variant="tertiary"
+          icon="gear"
+          >Gestisci Licenze</z-button
+        >
+      </z-myz-card-info>
+    </z-myz-card-dictionary>
+  `,args:{hideinfobtn:!0}};var l,s,c;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  render: args => html\`
+    <z-myz-card-dictionary
+      name="\${args.name}"
+      cover="\${args.cover}"
+      disabled="\${args.disabled}"
+      hideinfobtn="\${args.hideinfobtn}"
+      flipped="\${args.flipped}"
+      flipbuttonlabel="\${args.flipbuttonlabel}"
+    >
+      <z-myz-card-footer-sections>
+        <div slot="top">Vocabolario della lingua italiana</div>
+        <div slot="bottom">
+          <z-button
+            variant="primary"
+            style="width:100%;"
+            >VAI AL PRODOTTO</z-button
+          >
+        </div>
+      </z-myz-card-footer-sections>
+      <z-myz-card-info
+        slot="info"
+        data='{"author":"Nicola Zingarelli","year":"2021","title":"Vocabolario della lingua italiana","description":"A cura di Mario Cannella, Beata Lazzarini","onlineLicense":{"expiration":"31/12/2021","installations":"1"},"offlineLicense":{"expiration":"30/06/2021","installations":"2"}}'
+      >
+        <z-button
+          variant="tertiary"
+          icon="gear"
+          >Gestisci Licenze</z-button
+        >
+      </z-myz-card-info>
+    </z-myz-card-dictionary>
+  \`
+}`,...(c=(s=t.parameters)==null?void 0:s.docs)==null?void 0:c.source}}};var f,p,m;n.parameters={...n.parameters,docs:{...(f=n.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: args => html\`
+    <z-myz-card-dictionary
+      name="\${args.name}"
+      cover="\${args.cover}"
+      disabled="\${args.disabled}"
+      hideinfobtn="\${args.hideinfobtn}"
+      flipped="\${args.flipped}"
+      flipbuttonlabel="\${args.flipbuttonlabel}"
+    >
+      <z-myz-card-footer-sections>
+        <div slot="top">Vocabolario della lingua italiana</div>
+        <div slot="bottom">
+          <z-button
+            variant="primary"
+            style="width:100%;"
+            >VAI AL PRODOTTO</z-button
+          >
+        </div>
+      </z-myz-card-footer-sections>
+      <z-myz-card-info
+        slot="info"
+        data='{"author":"Nicola Zingarelli","year":"2021","title":"Vocabolario della lingua italiana","description":"A cura di Mario Cannella, Beata Lazzarini","onlineLicense":{"expiration":"31/12/2021","installations":"1"},"offlineLicense":{"expiration":"30/06/2021","installations":"2"}}'
+      >
+        <z-button
+          variant="tertiary"
+          icon="gear"
+          >Gestisci Licenze</z-button
+        >
+      </z-myz-card-info>
+    </z-myz-card-dictionary>
+  \`,
+  args: {
+    flipped: true
+  }
+}`,...(m=(p=n.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};var b,z,y;e.parameters={...e.parameters,docs:{...(b=e.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: args => html\`
+    <z-myz-card-dictionary
+      name="\${args.name}"
+      cover="\${args.cover}"
+      disabled="\${args.disabled}"
+      hideinfobtn="\${args.hideinfobtn}"
+      flipped="\${args.flipped}"
+      flipbuttonlabel="\${args.flipbuttonlabel}"
+    >
+      <z-myz-card-footer-sections>
+        <div slot="top">Vocabolario della lingua italiana</div>
+        <div slot="bottom">
+          <z-button
+            variant="primary"
+            style="width:100%;"
+            >VAI AL PRODOTTO</z-button
+          >
+        </div>
+      </z-myz-card-footer-sections>
+      <z-myz-card-info
+        slot="info"
+        data='{"author":"Nicola Zingarelli","year":"2021","title":"Vocabolario della lingua italiana","description":"A cura di Mario Cannella, Beata Lazzarini","onlineLicense":{"expiration":"31/12/2021","installations":"1"},"offlineLicense":{"expiration":"30/06/2021","installations":"2"}}'
+      >
+        <z-button
+          variant="tertiary"
+          icon="gear"
+          >Gestisci Licenze</z-button
+        >
+      </z-myz-card-info>
+    </z-myz-card-dictionary>
+  \`,
+  args: {
+    hideinfobtn: true
+  }
+}`,...(y=(z=e.parameters)==null?void 0:z.docs)==null?void 0:y.source}}};const G=["CardDefault","CardFlipped","CardNoInfoButton"];export{t as CardDefault,n as CardFlipped,e as CardNoInfoButton,G as __namedExportsOrder,R as default};
