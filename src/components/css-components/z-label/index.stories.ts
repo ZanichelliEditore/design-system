0a2e27700e9caa5fc36495a15274ddbbb10dde62
Michele Salvador <saldomik@gmail.com>
2025-05-06 15:04:15 +0200

    feat: add storybook doc

---
+++ src/components/css-components/z-label/index.stories.ts
@@ -0,0 +1,11 @@
+import {html} from "lit";
+
+export default {
+  title: "ZLabel",
+  argTypes: {},
+  args: {},
+};
+
+export const Default = {
+  render: () => html` <label class="z-label"> Etichetta </label> `,
+};
