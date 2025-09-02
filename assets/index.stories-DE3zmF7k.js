b7026bca8a81ce613865fbeb0721828765792c83
vpasquino <vpasquino@users.noreply.github.com>
2025-09-02 14:46:56 +0000

    deploy: 3145663c8ab9bd054db1be1e69fd5498e7922bbb

---
+++ assets/index.stories-DE3zmF7k.js
@@ -0,0 +1,7 @@
+import"./lit-element--ufgQf_M.js";import{x as s}from"./lit-html-rc9TYZL2.js";import"./index-B0gTjj-e.js";import"./chunk-L4EGOTBX-BzYVsoCE.js";import"./entry-preview-uRqU0LK7.js";import"./index-DrFu-skq.js";import"./index-BZ0rSFNp.js";const m={title:"Snowflakes/MyzCard/ZMyzCardList",component:"z-myz-card-list"},i={render:()=>s` <z-myz-card-list
+      listdata='[{"isLink":true,"value":"Leggi sul browser","url":"https://booktab.it/"},{"isLink":true,"value":"Sito e risorse del libro","url":"http://online.scuola.zanichelli.it/curtisinvitoblu/"},{"isLink":false,"value":"Programmazione"},{"isLink":false,"value":"Prove di verifica"},{"isLink":true,"value":"ZTE","url":"https://zte.zanichelli.it/"}]'
+    />`};var e,r,t;i.parameters={...i.parameters,docs:{...(e=i.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  render: () => html\` <z-myz-card-list
+      listdata='[{"isLink":true,"value":"Leggi sul browser","url":"https://booktab.it/"},{"isLink":true,"value":"Sito e risorse del libro","url":"http://online.scuola.zanichelli.it/curtisinvitoblu/"},{"isLink":false,"value":"Programmazione"},{"isLink":false,"value":"Prove di verifica"},{"isLink":true,"value":"ZTE","url":"https://zte.zanichelli.it/"}]'
+    />\`
+}`,...(t=(r=i.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const p=["ZMyzCardList"];export{i as ZMyzCardList,p as __namedExportsOrder,m as default};
