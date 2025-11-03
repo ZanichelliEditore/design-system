0aa09434ebfedf9e1c8c0fc3113baa657b699de7
FrancescoSanti <fsanti@zanichelli.it>
2025-11-03 10:04:33 +0100

    fix: handle empty rc version number

--- scripts/pre-release.sh
+++ scripts/pre-release.sh
@@ -4,7 +4,12 @@ set -e
 
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${GITHUB_ACTOR_ID}+${GITHUB_ACTOR}@users.noreply.github.com"
+
 yarn cache clean
 yarn install
 yarn build
 # yarn test
+
+# Required to use OpenID Connect feature
+echo "Update npm to the latest version"
+npm install -g npm@latest
