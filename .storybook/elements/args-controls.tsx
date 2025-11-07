f288bdeeff9aa8dc43fdee83192e1639c71d95fc
FrancescoSanti <fsanti@zanichelli.it>
2025-11-07 12:12:14 +0100

    fix: update Storybook to align version with that required by `addon-themes`

--- .storybook/elements/args-controls.tsx
+++ .storybook/elements/args-controls.tsx
@@ -1,10 +1,10 @@
 import {PresetColor, PureArgsTable, SortType} from "@storybook/blocks";
 import {Badge, Spaced} from "@storybook/components";
-import {includeConditionalArg} from "@storybook/csf";
 import {useArgTypes, useArgs, useGlobals, useParameter, useStorybookState} from "@storybook/manager-api";
 import {styled} from "@storybook/theming";
 import {type ArgTypes} from "@storybook/web-components";
 import React, {useEffect, useState} from "react";
+import {includeConditionalArg} from "storybook/internal/csf";
 
 export const ADDON_ID = "args-controls";
 export const PARAM_KEY = "addon-args-controls";
