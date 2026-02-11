3ffbcab5b013cf170808ba39064625e5a98cdfdd
DavideDA <DavideDA@users.noreply.github.com>
2026-02-11 10:09:13 +0000

    deploy: 73ef4f0bed1e48439d32af7324a41d5a91445616

---
+++ assets/index.stories-ml31_Brv.js
@@ -0,0 +1,18 @@
+import{x as c}from"./lit-element-DGXdXVoE.js";import{o as i}from"./style-map-DWZVjDvr.js";import"./index-8Lz41hcu.js";import"./directive-CF8sV3Lr.js";import"./chunk-L4EGOTBX-lo-pZyle.js";import"./entry-preview-Cx7Tr7Nx.js";import"./index-DrFu-skq.js";import"./index-BZ0rSFNp.js";const m={title:"ZBookCover",component:"z-book-cover",args:{cover:"https://staticmy.zanichelli.it/copertine/dashboard/m40002.9788808999825.jpg",bordered:!1,multiple:!1,"--z-book-cover-height":"378px","--z-book-cover-border-radius":"var(--border-radius)","--z-book-cover-stack-shift-x":"8px","--z-book-cover-stack-shift-y":"12px"}},r={render:o=>c`<z-book-cover
+      cover=${o.cover}
+      .bordered=${o.bordered}
+      .multiple=${o.multiple}
+      style=${i({"--z-book-cover-height":o["--z-book-cover-height"],"--z-book-cover-border-radius":o["--z-book-cover-border-radius"],"--z-book-cover-stack-shift-x":o["--z-book-cover-stack-shift-x"],"--z-book-cover-stack-shift-y":o["--z-book-cover-stack-shift-y"]})}
+    ></z-book-cover>`};var e,t,s;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  render: args => html\`<z-book-cover
+      cover=\${args.cover}
+      .bordered=\${args.bordered}
+      .multiple=\${args.multiple}
+      style=\${styleMap({
+    "--z-book-cover-height": args["--z-book-cover-height"],
+    "--z-book-cover-border-radius": args["--z-book-cover-border-radius"],
+    "--z-book-cover-stack-shift-x": args["--z-book-cover-stack-shift-x"],
+    "--z-book-cover-stack-shift-y": args["--z-book-cover-stack-shift-y"]
+  })}
+    ></z-book-cover>\`
+} satisfies Story`,...(s=(t=r.parameters)==null?void 0:t.docs)==null?void 0:s.source}}};const l=["Default"];export{r as Default,l as __namedExportsOrder,m as default};
