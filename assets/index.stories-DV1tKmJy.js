0b2e41ea2cc9cc350ab1c18813836f373cf75e32
DarioSacco <DarioSacco@users.noreply.github.com>
2026-03-12 13:23:27 +0000

    deploy: 3d8094a9158655c1973883df733785ae74a7d908

---
+++ assets/index.stories-DV1tKmJy.js
@@ -0,0 +1,9 @@
+import{x as i}from"./lit-element-DGXdXVoE.js";import{g as o}from"./index-2GKZ5e5J.js";import"./index-5LO31R1c.js";import"./chunk-L4EGOTBX-lo-pZyle.js";import"./entry-preview-Cx7Tr7Nx.js";import"./index-DrFu-skq.js";import"./index-BZ0rSFNp.js";import"./utils-DPTHofBv.js";import"./breakpoints-p3CVLX0Q.js";const C={title:"ZColorPicker",component:"z-color-picker",argTypes:{lng:{control:{type:"inline-radio"},options:["it","en"]},selectedColor:{control:{type:"select",labels:Object.fromEntries(Object.entries(o).map(([e,l])=>[e,l.it]))},options:Object.keys(o)}},args:{lng:"it",htmlAriaLabel:"Seleziona un colore",selectedColor:void 0,disableTransparent:!1}},r={render:e=>i`<z-color-picker
+      .selectedColor=${e.selectedColor}
+      .disableTransparent=${e.disableTransparent}
+    ></z-color-picker>`};var t,s,a;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: args => html\`<z-color-picker
+      .selectedColor=\${args.selectedColor}
+      .disableTransparent=\${args.disableTransparent}
+    ></z-color-picker>\`
+} satisfies Story`,...(a=(s=r.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};const u=["Default"];export{r as Default,u as __namedExportsOrder,C as default};
