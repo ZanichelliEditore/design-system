8588118368bd065bff2596d36ff869f6c66b38c4
DarioSacco <DarioSacco@users.noreply.github.com>
2026-04-02 13:25:39 +0000

    deploy: cd1da0dad066b7c1e1f041806c960c078c7185c6

---
+++ assets/index.stories-CksUrlQf.js
@@ -0,0 +1,145 @@
+import{x as p}from"./lit-element-DGXdXVoE.js";import{c as d,L as r,D as t,d as l}from"./index-2GKZ5e5J.js";import{g as v}from"./storybook-utils-DX4XXC0w.js";import"./index-C2gYISjQ.js";import"./index-BKPrwAZg.js";import"./index-Bt0MK5XT.js";import"./index-DN_DTmEP.js";import"./chunk-L4EGOTBX-lo-pZyle.js";import"./entry-preview-Cx7Tr7Nx.js";import"./index-DrFu-skq.js";import"./index-BZ0rSFNp.js";import"./index-Cr-W7by1.js";const L={title:"ZList/ZListGroup",component:"z-list-group",argTypes:{size:{control:{type:"inline-radio"},options:Object.values(d)},dividerType:{control:{type:"inline-radio"},options:Object.values(r)},dividerSize:{control:{type:"inline-radio"},options:Object.values(t)},dividerColor:v(),listType:{control:{type:"inline-radio"},options:Object.values(l)}},args:{size:d.SMALL,dividerType:r.HEADER,dividerSize:t.SMALL,dividerColor:"gray200",listType:l.NONE,role:"group"}},i={render:e=>p`
+    <z-list>
+      <z-list-group
+        size=${e.size}
+        divider-type=${e.dividerType}
+        divider-size=${e.dividerSize}
+        divider-color=${e.dividerColor}
+        list-type=${e.listType}
+        role=${e.role}
+      >
+        <div slot="header-title">Gruppo 1</div>
+        <z-list-element
+          size=${e.size}
+          list-type=${e.listType}
+          divider-type=${e.dividerType}
+          divider-size=${e.dividerSize}
+          divider-color=${e.dividerColor}
+          >Elemento 1</z-list-element
+        >
+        <z-list-element
+          size=${e.size}
+          list-type=${e.listType}
+          divider-type=${e.dividerType}
+          divider-size=${e.dividerSize}
+          divider-color=${e.dividerColor}
+          >Elemento 2</z-list-element
+        >
+        <z-list-element
+          size=${e.size}
+          list-type=${e.listType}
+          divider-type=${e.dividerType}
+          divider-size=${e.dividerSize}
+          divider-color=${e.dividerColor}
+          >Elemento 3</z-list-element
+        >
+      </z-list-group>
+      <z-list-group
+        size=${e.size}
+        divider-type=${e.dividerType}
+        divider-size=${e.dividerSize}
+        divider-color=${e.dividerColor}
+        list-type=${e.listType}
+        role=${e.role}
+      >
+        <div slot="header-title">Gruppo 1</div>
+        <z-list-element
+          size=${e.size}
+          list-type=${e.listType}
+          divider-type=${e.dividerType}
+          divider-size=${e.dividerSize}
+          divider-color=${e.dividerColor}
+          >Elemento 1</z-list-element
+        >
+        <z-list-element
+          size=${e.size}
+          list-type=${e.listType}
+          divider-type=${e.dividerType}
+          divider-size=${e.dividerSize}
+          divider-color=${e.dividerColor}
+          >Elemento 2</z-list-element
+        >
+        <z-list-element
+          size=${e.size}
+          list-type=${e.listType}
+          divider-type=${e.dividerType}
+          divider-size=${e.dividerSize}
+          divider-color=${e.dividerColor}
+          >Elemento 3</z-list-element
+        >
+      </z-list-group>
+    </z-list>
+  `};var o,s,n;i.parameters={...i.parameters,docs:{...(o=i.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: args => html\`
+    <z-list>
+      <z-list-group
+        size=\${args.size}
+        divider-type=\${args.dividerType}
+        divider-size=\${args.dividerSize}
+        divider-color=\${args.dividerColor}
+        list-type=\${args.listType}
+        role=\${args.role}
+      >
+        <div slot="header-title">Gruppo 1</div>
+        <z-list-element
+          size=\${args.size}
+          list-type=\${args.listType}
+          divider-type=\${args.dividerType}
+          divider-size=\${args.dividerSize}
+          divider-color=\${args.dividerColor}
+          >Elemento 1</z-list-element
+        >
+        <z-list-element
+          size=\${args.size}
+          list-type=\${args.listType}
+          divider-type=\${args.dividerType}
+          divider-size=\${args.dividerSize}
+          divider-color=\${args.dividerColor}
+          >Elemento 2</z-list-element
+        >
+        <z-list-element
+          size=\${args.size}
+          list-type=\${args.listType}
+          divider-type=\${args.dividerType}
+          divider-size=\${args.dividerSize}
+          divider-color=\${args.dividerColor}
+          >Elemento 3</z-list-element
+        >
+      </z-list-group>
+      <z-list-group
+        size=\${args.size}
+        divider-type=\${args.dividerType}
+        divider-size=\${args.dividerSize}
+        divider-color=\${args.dividerColor}
+        list-type=\${args.listType}
+        role=\${args.role}
+      >
+        <div slot="header-title">Gruppo 1</div>
+        <z-list-element
+          size=\${args.size}
+          list-type=\${args.listType}
+          divider-type=\${args.dividerType}
+          divider-size=\${args.dividerSize}
+          divider-color=\${args.dividerColor}
+          >Elemento 1</z-list-element
+        >
+        <z-list-element
+          size=\${args.size}
+          list-type=\${args.listType}
+          divider-type=\${args.dividerType}
+          divider-size=\${args.dividerSize}
+          divider-color=\${args.dividerColor}
+          >Elemento 2</z-list-element
+        >
+        <z-list-element
+          size=\${args.size}
+          list-type=\${args.listType}
+          divider-type=\${args.dividerType}
+          divider-size=\${args.dividerSize}
+          divider-color=\${args.dividerColor}
+          >Elemento 3</z-list-element
+        >
+      </z-list-group>
+    </z-list>
+  \`
+}`,...(n=(s=i.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};const f=["Default"];export{i as Default,f as __namedExportsOrder,L as default};
