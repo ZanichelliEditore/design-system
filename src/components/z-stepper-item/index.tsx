bfcfaa8800c26590b6637839a71aafdd644c2a58
nicole liggeri <nicole.liggeri@wondertech.web.com>
2026-07-14 16:27:06 +0200

    chore: handle label typography

--- src/components/z-stepper-item/index.tsx
+++ src/components/z-stepper-item/index.tsx
@@ -65,25 +65,44 @@ export class ZStepperItem implements ComponentInterface {
     };
   }
 
+  private getLabelClass(): Record<string, boolean> {
+    if (this.disabled) {
+      return {
+        "mobile-body-4": this.viewPortWidth === Device.MOBILE,
+        "tablet-body-4": this.viewPortWidth === Device.TABLET,
+        "desktop-body-3": this.viewPortWidth === Device.DESKTOP,
+        "wide-body-3": this.viewPortWidth === Device.DESKTOP_WIDE,
+      };
+    }
+
+    return {
+      "mobile-body-4-sb": this.viewPortWidth === Device.MOBILE,
+      "tablet-body-4-sb": this.viewPortWidth === Device.TABLET,
+      "desktop-body-3-sb": this.viewPortWidth === Device.DESKTOP,
+      "wide-body-3-sb": this.viewPortWidth === Device.DESKTOP_WIDE,
+    };
+  }
+
   render(): HTMLAnchorElement | HTMLButtonElement {
     return (
       <Host role="listitem">
         <button
-          class="stepper-item "
+          class="stepper-item"
           disabled={this.disabled}
           {...this.getAttributes()}
         >
           <div
             class={{
               "indicator": true,
-              "mobile-interactive-3-sb": this.viewPortWidth === Device.MOBILE || this.viewPortWidth === Device.TABLET,
-              "desktop-interactive-1-sb":
-                this.viewPortWidth === Device.DESKTOP || this.viewPortWidth === Device.DESKTOP_WIDE,
+              "mobile-interactive-3-sb": this.viewPortWidth === Device.MOBILE,
+              "tablet-interactive-3-sb": this.viewPortWidth === Device.TABLET,
+              "desktop-interactive-1-sb": this.viewPortWidth === Device.DESKTOP,
+              "wide-interactive-1-sb": this.viewPortWidth === Device.DESKTOP_WIDE,
             }}
           >
             {this.checked ? <z-icon name="checkmark" /> : this.index}
           </div>
-          <span>
+          <span class={this.getLabelClass()}>
             <slot />
           </span>
         </button>
